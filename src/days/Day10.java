1a078f7a8b7e82391b06dbb4115947e1c8a35096
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-11-29 17:37:29 +0100

    day10

---
+++ src/days/Day10.java
@@ -0,0 +1,121 @@
+package days;
+
+import setup.Day;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Day10 extends Day {
+
+    List<Point> points;
+    int seconds = 0;
+
+    @Override
+    public void processInput() {
+        Pattern p = Pattern.compile("\\D*?(-?\\d+)\\D*?(-?\\d+)\\D*?(-?\\d+)\\D*?(-?\\d+)\\D*?");
+        points = Arrays.stream(input.split("\n"))
+                .map(p::matcher)
+                .peek(Matcher::matches)
+                .map(m -> new Point(
+                        Integer.parseInt(m.group(1).trim()),
+                        Integer.parseInt(m.group(2).trim()),
+                        Integer.parseInt(m.group(3).trim()),
+                        Integer.parseInt(m.group(4).trim())
+                )).collect(Collectors.toList());
+    }
+
+    void makeGrid(int oldHeight, Set<Position> oldPositions) {
+        Set<Position> positions = points.stream().map(p -> p.pos).collect(Collectors.toSet());
+
+        int minY = Collections.min(positions, compareY).y;
+        int maxY = Collections.max(positions, compareY).y;
+
+        int height = maxY - minY;
+
+        if (oldHeight < height) {
+            printGrid(oldPositions);
+        } else {
+            points.forEach(Point::move);
+            makeGrid(height, positions);
+        }
+    }
+
+    void printGrid(Set<Position> positions) {
+        int minX = Collections.min(positions, compareX).x;
+        int minY = Collections.min(positions, compareY).y;
+        int maxX = Collections.max(positions, compareX).x;
+        int maxY = Collections.max(positions, compareY).y;
+
+        for (int y = minY; y < maxY +1; y++) {
+            for (int x = minX; x < maxX +1; x++) {
+                System.out.print(positions.contains(new Position(x, y)) ? "#" : ".");
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public void part1() {
+        int oldHeight = Integer.MAX_VALUE;
+        Set<Position> oldPositions = null;
+
+        while (true) {
+            Set<Position> positions = points.stream().map(p -> p.pos).collect(Collectors.toSet());
+            int minY = Collections.min(positions, compareY).y;
+            int maxY = Collections.max(positions, compareY).y;
+            int height = maxY - minY;
+
+            if (oldHeight < height) {
+                printGrid(oldPositions);
+                break;
+            } else {
+                oldHeight = height;
+                oldPositions = positions;
+                points.forEach(Point::move);
+                seconds++;
+            }
+        }
+    }
+
+    @Override
+    public void part2() {
+        System.out.println(seconds - 1);
+    }
+
+    @Override
+    public int getDay() {
+        return 10;
+    }
+
+    @Override
+    public boolean isTest() {
+        return false;
+    }
+
+    private static class Point {
+        Position pos;
+        final int vx, vy;
+
+        Point(int x, int y, int vx, int vy) {
+            this.vx = vx;
+            this.vy = vy;
+            this.pos = new Position(x, y);
+        }
+
+        void move() {
+            pos = new Position(pos.x + vx, pos.y + vy);
+        }
+
+        @Override
+        public String toString() {
+            return "" + pos;
+        }
+    }
+
+    private static record Position(int x, int y)  {}
+
+    private static final Comparator<Position> compareX = Comparator.comparingInt(o -> o.x);
+    private static final Comparator<Position> compareY = Comparator.comparingInt(o -> o.y);
+}
