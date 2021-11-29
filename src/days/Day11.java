3ed903ffbcb5c6d5382e9ccf53ccadec2dba5dd1
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-11-29 20:36:46 +0100

    day11 p2

--- src/days/Day11.java
+++ src/days/Day11.java
@@ -2,7 +2,6 @@ package days;
 
 import setup.Day;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +9,7 @@ import java.util.Map;
 public class Day11 extends Day {
 
     private int serialNumber;
+    private Map<Pair, Integer> powerLevels;
 
     @Override
     public void processInput() {
@@ -17,8 +17,8 @@ public class Day11 extends Day {
     }
 
     private int getPowerLevel(int x, int y) {
-        int rackID = x+10;
-        int powerLevel = rackID*y;
+        int rackID = x + 10;
+        int powerLevel = rackID * y;
         powerLevel += serialNumber;
         powerLevel *= rackID;
 
@@ -31,11 +31,11 @@ public class Day11 extends Day {
 
     @Override
     public void part1() {
-        Map<Pair, Integer> power = new HashMap<>();
+        powerLevels = new HashMap<>();
 
         for (int x = 1; x < 301; x++) {
             for (int y = 1; y < 301; y++) {
-                power.put(new Pair(x, y), getPowerLevel(x, y));
+                powerLevels.put(new Pair(x, y), getPowerLevel(x, y));
             }
         }
 
@@ -53,7 +53,7 @@ public class Day11 extends Day {
                         new Pair(x + 1, y + 2),
                         new Pair(x + 2, y + 2)
                 );
-                int sum = coords.stream().mapToInt(power::get).sum();
+                int sum = coords.stream().mapToInt(powerLevels::get).sum();
                 squarePower.put(new Pair(x, y), sum);
             }
         }
@@ -63,6 +63,36 @@ public class Day11 extends Day {
 
     @Override
     public void part2() {
+        Map<Triple, Long> squarePowerLevels = new HashMap<>();
+        powerLevels.forEach((k, v) -> squarePowerLevels.put(new Triple(k.x, k.y, 1), Long.valueOf(v)));
+
+        for (int y = 1; y < 6; y++) {
+            for (int x = 1; x < 6; x++) {
+                System.out.print(" " + squarePowerLevels.get(new Triple(x, y, 1)) + " ");
+            }
+            System.out.println();
+        }
+
+        int gridSize = 300;
+        for (int s = 2; s < gridSize + 1; s++) {
+            for (int x = 1; x <= gridSize - s + 1; x++) {
+                for (int y = 1; y <= gridSize - s + 1; y++) {
+                    long a, b, c, d, e;
+
+                    a = squarePowerLevels.get(new Triple(x + s - 1, y, 1));
+                    b = squarePowerLevels.get(new Triple(x, y + s - 1, 1));
+                    c = squarePowerLevels.get(new Triple(x, y, s - 1));
+                    d = squarePowerLevels.get(new Triple(x + 1, y + 1, s - 1));
+                    e = s > 2 ?  squarePowerLevels.get(new Triple(x + 1, y + 1, s - 2)) : 0;
+
+                    squarePowerLevels.put(new Triple(x, y, s), a + b + c + d - e);
+                }
+            }
+        }
+
+        Map.Entry<Triple, Long> tripleLongEntry = squarePowerLevels.entrySet().stream().max(Map.Entry.comparingByValue()).get();
+        System.out.println(tripleLongEntry.getKey());
+        System.out.println(tripleLongEntry.getValue());
 
     }
 
@@ -76,5 +106,9 @@ public class Day11 extends Day {
         return false;
     }
 
-    private static record Pair(int x, int y) {}
+    private static record Pair(int x, int y) {
+    }
+
+    private static record Triple(int x, int y, int size) {
+    }
 }
