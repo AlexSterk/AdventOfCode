a3bfc5afc5df5f1163a61ba152ee4c6ffa9aaa55
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-11-30 21:16:35 +0100

    Day14 p2

--- src/days/Day14.java
+++ src/days/Day14.java
@@ -2,42 +2,44 @@ package days;
 
 import setup.Day;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Day14 extends Day {
 
-    private List<Integer> elves;
-    private List<Integer> recipes;
+    private int[] elves;
+    private StringBuilder recipes;
 
-    private int numRecipes;
+    private Integer numRecipes;
 
     @Override
     public void processInput() {
-        recipes = new ArrayList<>(List.of(3, 7));
-        elves = new ArrayList<>(List.of(0, 1));
+        recipes = new StringBuilder("37");
+        elves = new int[]{0, 1};
         numRecipes = Integer.parseInt(input.trim());
     }
 
     @Override
     public void part1() {
-        while (recipes.size() < numRecipes + 10) {
-            int sum = recipes.get(elves.get(0)) + recipes.get(elves.get(1));
-            List<Integer> digits = Arrays.stream(Integer.toString(sum).split("")).map(Integer::parseInt).collect(Collectors.toList());
-            recipes.addAll(digits);
-            for (int i = 0; i < elves.size(); i++) {
-                Integer currentRecipeIndex = elves.get(i);
-                Integer currentRecipeScore = recipes.get(currentRecipeIndex);
-                elves.set(i, (currentRecipeIndex + 1 + currentRecipeScore) % recipes.size());
-            }
+        while (recipes.length() < numRecipes + 10) {
+            createRecipes();
         }
-        recipes.subList(numRecipes, numRecipes + 10).forEach(System.out::print);
-        System.out.println();
+        System.out.println(recipes.substring(numRecipes, numRecipes + 10));
+    }
+
+    private void createRecipes() {
+        int sum = Character.getNumericValue(recipes.charAt(elves[0])) + Character.getNumericValue(recipes.charAt(elves[1]));
+        recipes.append(sum);
+        for (int i = 0; i < elves.length; i++) {
+            Integer currentRecipeIndex = elves[i];
+            Integer currentRecipeScore = Character.getNumericValue(recipes.charAt(currentRecipeIndex));
+            elves[i] = (currentRecipeIndex + 1 + currentRecipeScore) % recipes.length();
+        }
     }
 
     @Override
     public void part2() {
-
+        for (int i = 0; i < 30_000_000; i++) {
+            createRecipes();
+        }
+        System.out.println(recipes.indexOf(numRecipes.toString()));
     }
 
     @Override
