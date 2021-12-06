48a6e37c5d8ff1d2fff19bba66d0ae0a710c811e
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-06 11:23:43 +0100

    Day6 part2

--- src/main/java/days/Day6.java
+++ src/main/java/days/Day6.java
@@ -5,6 +5,7 @@ import setup.Day;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Day6 extends Day {
     private List<Integer> fishes;
@@ -17,7 +18,6 @@ public class Day6 extends Day {
     @Override
     public Object part1() {
         for (int n = 0; n < 80; n++) {
-//            System.out.println(fishes);
             int size = fishes.size();
             for (int i = 0; i < size; i++) {
                 Integer integer = fishes.get(i);
@@ -33,19 +33,24 @@ public class Day6 extends Day {
 
     @Override
     public Object part2() {
-        for (int n = 0; n < 80; n++) {
-//            System.out.println(fishes);
-            int size = fishes.size();
-            for (int i = 0; i < size; i++) {
-                Integer integer = fishes.get(i);
-                if (integer == 0) {
-                    fishes.set(i, 6);
-                    fishes.add(8);
-                } else fishes.set(i, integer - 1);
-            }
+        long spawning;
+        long[] populations = IntStream.rangeClosed(0, 8)
+                .mapToLong(i -> fishes.stream().filter(x -> x == i).count())
+                .toArray();
+        for (int i = 0; i < 256; i++) {
+            spawning = populations[0];
+            populations[0] = populations[1];
+            populations[1] = populations[2];
+            populations[2] = populations[3];
+            populations[3] = populations[4];
+            populations[4] = populations[5];
+            populations[5] = populations[6];
+            populations[6] = populations[7] + spawning;
+            populations[7] = populations[8];
+            populations[8] = spawning;
         }
 
-        return null;
+        return Arrays.stream(populations).sum();
     }
 
     @Override
@@ -66,5 +71,10 @@ public class Day6 extends Day {
     @Override
     public String partOneSolution() {
         return "353274";
+    }
+
+    @Override
+    public String partTwoSolution() {
+        return "1609314870967";
     }
 }
