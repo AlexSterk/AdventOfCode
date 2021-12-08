979eca4e5dbe2009603e86da12bf3bf512e99ae8
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-08 14:30:00 +0100

    Day8 p2

--- src/main/java/days/Day8.java
+++ src/main/java/days/Day8.java
@@ -5,25 +5,9 @@ import util.Pair;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day8 extends Day {
-    private static final Map<Integer, Boolean[]> SEGMENTS_TO_DISPLAY = new HashMap<>();
-
-    static {
-        SEGMENTS_TO_DISPLAY.put(0, new Boolean[]{true, true, true, false, true, true, true});
-        SEGMENTS_TO_DISPLAY.put(1, new Boolean[]{false, false, true, false, false, true, false});
-        SEGMENTS_TO_DISPLAY.put(2, new Boolean[]{true, false, true, true, true, false, true});
-        SEGMENTS_TO_DISPLAY.put(3, new Boolean[]{true, false, true, true, false, true, true});
-        SEGMENTS_TO_DISPLAY.put(4, new Boolean[]{false, true, true, true, false, true, false});
-        SEGMENTS_TO_DISPLAY.put(5, new Boolean[]{true, true, false, true, false, true, true});
-        SEGMENTS_TO_DISPLAY.put(6, new Boolean[]{true, true, false, true, true, true, false});
-        SEGMENTS_TO_DISPLAY.put(7, new Boolean[]{true, false, true, false, false, true, false});
-        SEGMENTS_TO_DISPLAY.put(8, new Boolean[]{true, true, true, true, true, true, true});
-        SEGMENTS_TO_DISPLAY.put(9, new Boolean[]{true, true, true, true, false, true, true});
-    }
-
     private List<Pair<List<String>, List<String>>> displays;
 
     @Override
@@ -35,7 +19,7 @@ public class Day8 extends Day {
             String patterns = split[0].trim();
             String input = split[1].trim();
 
-            return new Pair<>(List.of(patterns.split(" ")), List.of(input.split(" ")));
+            return new Pair<>(Stream.of(patterns.split(" ")).sorted(Comparator.comparing(String::length)).toList(), List.of(input.split(" ")));
         }).toList();
     }
 
@@ -64,7 +48,52 @@ public class Day8 extends Day {
 
     @Override
     public Object part2() {
-        return null;
+        Map<String, Integer> originalSignals = new HashMap<>();
+        originalSignals.put("abcefg", 0);
+        originalSignals.put("cf", 1);
+        originalSignals.put("acdeg", 2);
+        originalSignals.put("acdfg", 3);
+        originalSignals.put("bcdf", 4);
+        originalSignals.put("abdfg", 5);
+        originalSignals.put("abdefg", 6);
+        originalSignals.put("acf", 7);
+        originalSignals.put("abcdefg", 8);
+        originalSignals.put("abcdfg", 9);
+
+        int sum = 0;
+
+        for (Pair<List<String>, List<String>> display : displays) {
+            Map<Character, Character> mapping = new HashMap<>();
+            Map<Character, Set<String>> containing = new HashMap<>();
+            for (Character c : new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g'}) {
+                Set<String> has = display.a().stream().filter(s -> s.indexOf(c) >= 0).collect(Collectors.toSet());
+                containing.put(c, has);
+            }
+            for (Map.Entry<Character, Set<String>> e : containing.entrySet()) {
+                if (e.getValue().size() == 6) mapping.put(e.getKey(), 'b');
+                if (e.getValue().size() == 9) mapping.put(e.getKey(), 'f');
+                if (e.getValue().size() == 4) mapping.put(e.getKey(), 'e');
+                if (e.getValue().size() == 8) {
+                    if (e.getValue().stream().map(String::length).toList().contains(2)) mapping.put(e.getKey(), 'c');
+                    else mapping.put(e.getKey(), 'a');
+                }
+                if (e.getValue().size() == 7) {
+                    if (e.getValue().stream().map(String::length).toList().contains(4)) mapping.put(e.getKey(), 'd');
+                    else mapping.put(e.getKey(), 'g');
+                }
+            }
+            String number = display.b().stream().map(pattern -> {
+                char[] chars = pattern.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    chars[i] = mapping.get(chars[i]);
+                }
+                Arrays.sort(chars);
+                return new String(chars);
+            }).map(originalSignals::get).map(Object::toString).collect(Collectors.joining());
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 
     @Override
@@ -80,5 +109,10 @@ public class Day8 extends Day {
     @Override
     public String partOneSolution() {
         return "504";
+    }
+
+    @Override
+    public String partTwoSolution() {
+        return "1073431";
     }
 }
