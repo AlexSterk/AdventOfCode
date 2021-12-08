8d371baf5ce45ea8e5d5babfd5d96ec2771f83d4
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-08 03:14:00 +0100

    Day21 p2

--- src/days/Day21.java
+++ src/days/Day21.java
@@ -2,6 +2,7 @@ package days;
 
 import setup.Day;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.List;
@@ -24,7 +25,6 @@ public class Day21 extends Day {
         registers[0] = 15615244;
         while (registers[ip] < instructions.size()) {
             Day16.Instruction inst = instructions.get((int) registers[ip]);
-//            System.out.println(registers[ip] + " " + inst);
             Day16.Instruction.execute(inst, registers);
             registers[ip]++;
         }
@@ -35,7 +35,21 @@ public class Day21 extends Day {
 
     @Override
     public Object part2() {
-        return null;
+        long[] registers = new long[6];
+        registers[0] = -1;
+        List<Long> valuesOfR5 = new ArrayList<>();
+        while (registers[ip] < instructions.size()) {
+            if (registers[ip] == 28) {
+                long R5 = registers[5];
+                if (valuesOfR5.contains(R5))
+                    return valuesOfR5.get(valuesOfR5.size() - 1);
+                else valuesOfR5.add(R5);
+            }
+            Day16.Instruction inst = instructions.get((int) registers[ip]);
+            Day16.Instruction.execute(inst, registers);
+            registers[ip]++;
+        }
+        return registers[0];
     }
 
     @Override
