6b6c979f12d3d2703a1374b443fdd91bcaf44a0b
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-10 12:15:04 +0100

    Day22 commit what i have

--- src/days/Day22.java
+++ src/days/Day22.java
@@ -1,10 +1,9 @@
 package days;
 
 import setup.Day;
-import util.Line;
 import util.Line.Point;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Day22 extends Day {
     private int depth;
@@ -13,6 +12,8 @@ public class Day22 extends Day {
     private Integer[][] riskLevel;
 
     private Point target;
+    private int height;
+    private int width;
 
     @Override
     public void processInput() {
@@ -21,32 +22,45 @@ public class Day22 extends Day {
         String[] coords = lines[1].substring(8).split(",");
         target = new Point(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
 
-        geologicIndex = new Integer[target.y() + 1][target.x() + 1];
-        erosionLevels = new Integer[target.y() + 1][target.x() + 1];
-        riskLevel = new Integer[target.y() + 1][target.x() + 1];
+        height = target.y() + 1000;
+        width = target.x() + 1000;
+        geologicIndex = new Integer[height][width];
+        erosionLevels = new Integer[height][width];
+        riskLevel = new Integer[height][width];
     }
 
     @Override
     public Object part1() {
-        for (int y = 0; y <= target.y(); y++) {
-            for (int x = 0; x <= target.x(); x++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 int geoIndex;
                 if (x == 0 && y == 0) geoIndex = 0;
                 if (x == target.x() && y == target.y()) geoIndex = 0;
                 else if (y == 0) geoIndex = x * 16807;
                 else if (x == 0) geoIndex = y * 48271;
-                else geoIndex = erosionLevels[y][x-1] * erosionLevels[y - 1][x];
+                else geoIndex = erosionLevels[y][x - 1] * erosionLevels[y - 1][x];
                 geologicIndex[y][x] = geoIndex;
                 erosionLevels[y][x] = (geologicIndex[y][x] + depth) % 20183;
                 riskLevel[y][x] = erosionLevels[y][x] % 3;
             }
         }
 
-        return Arrays.stream(riskLevel).flatMap(Arrays::stream).mapToInt(i -> i).sum();
+        return Arrays.stream(riskLevel)
+                .limit(target.y() + 1)
+                .flatMap(array -> Arrays.stream(array).limit(target.x() + 1))
+                .mapToInt(i -> i).sum();
     }
 
     @Override
     public Object part2() {
+        Node[][] nodes = new Node[height][width];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                nodes[y][x] = new Node(x, y, null);
+            }
+        }
+
 
 
         return null;
@@ -59,6 +73,31 @@ public class Day22 extends Day {
 
     @Override
     public boolean isTest() {
-        return false;
+        return true;
+    }
+
+    private enum Tool {
+        TORCH,
+        GEAR,
+        NEITHER;
+    }
+
+    private record Graph(Set<Node> nodes, Map<Edge, Integer> weights) {
+
+    }
+
+    private record Node(int x, int y, Tool tool) {
+
+    }
+
+    private static class Edge extends HashSet<Node> {
+        public Edge(Node from, Node to) {
+            this.add(from);
+            this.add(to);
+        }
+
+        public Edge(Node cycle) {
+            this.add(cycle);
+        }
     }
 }
