030074fc4f488366e634b6043291769834a4b87a
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-10 20:12:03 +0100

    Use Graph

--- src/days/Day22.java
+++ src/days/Day22.java
@@ -1,10 +1,11 @@
 package days;
 
 import setup.Day;
+import util.Graph;
 import util.Line.Point;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Set;
 
 public class Day22 extends Day {
     private int depth;
@@ -36,7 +37,7 @@ public class Day22 extends Day {
             for (int x = 0; x < width; x++) {
                 int geoIndex;
                 if (x == 0 && y == 0) geoIndex = 0;
-                if (x == target.x() && y == target.y()) geoIndex = 0;
+                else if (x == target.x() && y == target.y()) geoIndex = 0;
                 else if (y == 0) geoIndex = x * 16807;
                 else if (x == 0) geoIndex = y * 48271;
                 else geoIndex = erosionLevels[y][x - 1] * erosionLevels[y - 1][x];
@@ -54,54 +55,38 @@ public class Day22 extends Day {
 
     @Override
     public Object part2() {
-        Graph g = new Graph();
+        Graph<Node> graph = new Graph<>();
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                Node a = null, b = null;
                 switch (riskLevel[y][x]) {
                     case 0 -> {
-                        g.put(x, y, Tool.GEAR);
-                        g.put(x, y, Tool.TORCH);
+                        a = new Node(x, y, Tool.GEAR);
+                        b = new Node(x, y, Tool.TORCH);
                     }
                     case 1 -> {
-                        g.put(x, y, Tool.GEAR);
-                        g.put(x, y, Tool.NEITHER);
+                        a = new Node(x, y, Tool.GEAR);
+                        b = new Node(x, y, Tool.NEITHER);
                     }
                     case 2 -> {
-                        g.put(x, y, Tool.NEITHER);
-                        g.put(x, y, Tool.TORCH);
+                        a = new Node(x, y, Tool.NEITHER);
+                        b = new Node(x, y, Tool.TORCH);
                     }
                 }
+                graph.addNode(a);
+                graph.addNode(b);
+                graph.addEdge(a, b, 7);
             }
         }
 
-        Node start = new Node(0, 0, Tool.TORCH);
-        Node end = new Node(target.x(), target.y(), Tool.TORCH);
-
-        Map<Node, Integer> dist = new HashMap<>();
-        dist.put(start, 0);
-        PriorityQueue<Node> Q = new PriorityQueue<>(Comparator.comparing(dist::get));
-
-        for (Node node : g.getAllNodes()) {
-            if (!node.equals(start)) {
-                dist.put(node, Integer.MAX_VALUE);
+        for (Node n1 : graph.nodes()) {
+            for (Node n2 : n1.neighbours()) {
+                if (graph.nodes().contains(n2)) graph.addEdge(n1, n2, 1);
             }
         }
 
-        Q.offer(start);
-
-        while (!Q.isEmpty()) {
-            Node u = Q.poll();
-            for (Node v : g.getNeighbours(u)) {
-                int alt = dist.get(u) + g.getWeight(u, v);
-                if (alt < dist.get(v)) {
-                    dist.put(v, alt);
-                    Q.offer(v);
-                }
-            }
-        }
-
-        return dist.get(end);
+        return graph.getDistance(new Node(0, 0, Tool.TORCH), new Node(target.x(), target.y(), Tool.TORCH));
     }
 
     @Override
@@ -117,63 +102,33 @@ public class Day22 extends Day {
     private enum Tool {
         TORCH,
         GEAR,
-        NEITHER;
+        NEITHER
     }
 
-    private record Graph(Map<Integer, Map<Integer, Map<Tool, Node>>> nodes) {
-        private Graph() {
-            this(new HashMap<>());
+    private record Node(int x, int y, Tool t) {
+        private Node up() {
+            return new Node(x, y - 1, t);
         }
 
-        private Set<Node> getAllNodes() {
-            return nodes.values()
-                    .stream()
-                    .flatMap(m -> m.values().stream())
-                    .flatMap(m -> m.values().stream())
-                    .collect(Collectors.toSet());
+        private Node down() {
+            return new Node(x, y + 1, t);
         }
 
-        private void put(int x, int y, Tool t) {
-            if (!nodes.containsKey(x)) {
-                nodes.put(x, new HashMap<>());
-            }
-            if (!nodes.get(x).containsKey(y)) {
-                nodes.get(x).put(y, new HashMap<>());
-            }
-            nodes.get(x).get(y).put(t, new Node(x, y, t));
+        private Node left() {
+            return new Node(x - 1, y, t);
         }
 
-        private Node get(int x, int y, Tool t) {
-            if (!nodes.containsKey(x)) return null;
-            if (!nodes.get(x).containsKey(y)) return null;
-            return nodes.get(x).get(y).getOrDefault(t, null);
+        private Node right() {
+            return new Node(x + 1, y, t);
         }
 
-        private Set<Node> getNeighbours(Node n) {
-            Set<Node> neighbours = new HashSet<>();
-
-            neighbours.add(this.get(n.x - 1, n.y, n.tool));
-            neighbours.add(this.get(n.x + 1, n.y, n.tool));
-            neighbours.add(this.get(n.x, n.y + 1, n.tool));
-            neighbours.add(this.get(n.x, n.y - 1, n.tool));
-
-            neighbours.add(this.get(n.x, n.y, Tool.GEAR));
-            neighbours.add(this.get(n.x, n.y, Tool.TORCH));
-            neighbours.add(this.get(n.x, n.y, Tool.NEITHER));
-
-            neighbours.remove(n);
-            neighbours.removeIf(Objects::isNull);
-
-            return neighbours;
+        private Set<Node> neighbours() {
+            return Set.of(
+                    up(),
+                    down(),
+                    left(),
+                    right()
+            );
         }
-
-        private int getWeight(Node from, Node to) {
-            if (from.x == to.x && from.y == to.y) return 7;
-            else return 1;
-        }
-    }
-
-    private record Node(int x, int y, Tool tool) {
-
     }
 }
