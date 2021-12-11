dff715c46e772ab8e85edcc1b5af0cf5cdb53e7b
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-11 18:27:49 +0100

    Day6 p1 and 2

--- src/main/java/days/Day6.java
+++ src/main/java/days/Day6.java
@@ -2,20 +2,41 @@ package days;
 
 import setup.Day;
 
+import java.util.*;
+
 public class Day6 extends Day {
+    private List<Integer> banks;
+    private List<List<Integer>> oldConfigs;
+
     @Override
     public void processInput() {
-
+        banks = new ArrayList<>();
+        for (String s : input.trim().split("\\s+")) {
+            banks.add(Integer.parseInt(s));
+        }
     }
 
     @Override
     public Object part1() {
-        return null;
+        oldConfigs = new ArrayList<>();
+        while (!oldConfigs.contains(banks)) {
+            oldConfigs.add(List.copyOf(banks));
+            int max = Collections.max(banks);
+            int index = banks.indexOf(max);
+            banks.set(index, 0);
+            while (max > 0) {
+                index = (index + 1) % banks.size();
+                banks.set(index, banks.get(index) + 1);
+                max--;
+            }
+        }
+
+        return oldConfigs.size();
     }
 
     @Override
     public Object part2() {
-        return null;
+        return oldConfigs.size() - oldConfigs.indexOf(banks);
     }
 
     @Override
