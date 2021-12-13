e7cf04c9de591a98d0af2b52b268a860cd4179ff
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-13 19:57:19 +0100

    Day17 p2

--- src/main/java/days/Day17.java
+++ src/main/java/days/Day17.java
@@ -19,19 +19,23 @@ public class Day17 extends Day {
         list.add(0);
         int cur = 0;
         for (int i = 1; i <= 2017; i++) {
-            for (int j = 0; j < steps; j++) {
-                cur = (cur + 1) % list.size();
-            }
-            cur++;
+            cur = (cur + steps) % list.size() + 1;
             list.add(cur, i);
         }
-
         return list.get(list.indexOf(2017) + 1);
     }
 
     @Override
     public Object part2() {
-        return null;
+        // 0 always in index 0 so we need to find index 1
+        int cur = 0;
+        int v = 0;
+        for (int i = 1; i < 50_000_000; i++) {
+            cur = (cur + steps) % i + 1;
+            if (cur == 1) v = i;
+        }
+
+        return v;
     }
 
     @Override
