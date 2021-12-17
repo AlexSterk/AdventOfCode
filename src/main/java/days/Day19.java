15ac6071ae53961ffd66a6827dfe55cf756d9eb0
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-17 02:17:31 +0100

    Day19 p1

---
+++ src/main/java/days/Day19.java
@@ -0,0 +1,93 @@
+package days;
+
+import setup.Day;
+import util.Line.Point;
+
+import java.util.Arrays;
+
+public class Day19 extends Day {
+    private char[][] routing;
+    private Point start;
+
+    private static final Point DOWN = new Point(0, 1);
+    private static final Point UP = new Point(0, -1);
+    private static final Point LEFT = new Point(-1, 0);
+    private static final Point RIGHT = new Point(1, 0);
+
+    private Point direction;
+
+    @Override
+    public void processInput() {
+        String[] lines = input.split("\n");
+        int maxLength = Arrays.stream(lines).mapToInt(String::length).max().getAsInt();
+        routing = new char[lines.length][maxLength];
+        for (int y = 0; y < lines.length; y++) {
+            for (int x = 0; x < lines[y].length(); x++) {
+                char c = lines[y].charAt(x);
+                if (c != ' ') {
+                    routing[y][x] = c;
+                    if (y == 0) start = new Point(x, y);
+                }
+            }
+        }
+        direction = DOWN;
+    }
+
+    @Override
+    public Object part1() {
+        Point current = start;
+        StringBuilder s = new StringBuilder();
+        while (true) {
+            if (canMove(current, direction)) {
+                current = current.add(direction);
+                char c = routing[current.y()][current.x()];
+                if (Character.isAlphabetic(c)) s.append(c);
+            } else {
+                if (canMove(current, turnLeft(direction))) {
+                    direction = turnLeft(direction);
+                }
+                else if (canMove(current, turnRight(direction))) {
+                    direction = turnRight(direction);
+                }
+                else break;
+            }
+        }
+        return s.toString();
+    }
+
+    private boolean canMove(Point current, Point dir) {
+        var add = current.add(dir);
+        return add.x() >= 0 && add.x() < routing[0].length && add.y() >= 0 && add.y() < routing.length && routing[add.y()][add.x()] != 0;
+    }
+
+    private static Point turnLeft(Point dir) {
+        /*
+        0,1 --> 1,0
+        1,0 --> 0,-1
+         */
+        return new Point(dir.y(), -dir.x());
+    }
+
+    private static Point turnRight(Point dir) {
+        /*
+        0,1 --> -1,0
+        -1,0 --> 0,-1
+         */
+        return new Point(-dir.y(), dir.x());
+    }
+
+    @Override
+    public Object part2() {
+        return null;
+    }
+
+    @Override
+    public int getDay() {
+        return 19;
+    }
+
+    @Override
+    public boolean isTest() {
+        return false;
+    }
+}
