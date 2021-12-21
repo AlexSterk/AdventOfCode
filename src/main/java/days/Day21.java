59f43e0b64df41f259bc78e48c3a262d4b875ec4
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2021-12-21 15:11:32 +0100

    Day21 p2

--- src/main/java/days/Day21.java
+++ src/main/java/days/Day21.java
@@ -2,20 +2,25 @@ package days;
 
 import setup.Day;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day21 extends Day {
-    private int playerOnePosition, playerTwoPosition, playerOneScore, playerTwoScore;
+    private int playerOnePosition;
+    private int playerTwoPosition;
 
     @Override
     public void processInput() {
         String[] lines = input.split("\n");
         playerOnePosition = Integer.parseInt(lines[0].substring(28));
         playerTwoPosition = Integer.parseInt(lines[1].substring(28));
-
-        playerOneScore = 0; playerTwoScore = 0;
     }
 
     @Override
     public Object part1() {
+        int playerOneScore = 0;
+        int playerTwoScore = 0;
         Die die = new Die();
 
         while (true) {
@@ -35,8 +40,37 @@ public class Day21 extends Day {
     }
 
     @Override
+    public boolean resetForPartTwo() {
+        return true;
+    }
+
+    @Override
     public Object part2() {
-        return null;
+        Map<Integer, Integer> diceCounts = new HashMap<>();
+        for (int i = 1; i <= 3; i++)
+            for (int k = 1; k <= 3; k++)
+                for (int j = 1; j <= 3; j++) {
+                    diceCounts.merge(i + k + j, 1, Integer::sum);
+                }
+
+        Map<Game, BigDecimal> games = new HashMap<>();
+        games.put(new Game(playerOnePosition, 0, playerTwoPosition, 0, 1), BigDecimal.ONE);
+
+        while (games.keySet().stream().anyMatch(g -> !g.gameOver())) {
+            Map<Game, BigDecimal> newGames = new HashMap<>();
+            games.forEach((g, cur) -> {
+                if (g.gameOver()) {
+                    newGames.merge(g, cur, BigDecimal::add);
+                } else
+                    diceCounts.forEach((d, c) -> newGames.merge(g.turn(d), cur.multiply(BigDecimal.valueOf(c)), BigDecimal::add));
+            });
+            games = newGames;
+        }
+
+        return Long.max(
+                games.keySet().stream().filter(g -> g.p1Score >= 21).map(games::get).mapToLong(BigDecimal::longValue).sum(),
+                games.keySet().stream().filter(g -> g.p2Score >= 21).map(games::get).mapToLong(BigDecimal::longValue).sum()
+        );
     }
 
     @Override
@@ -54,6 +88,11 @@ public class Day21 extends Day {
         return "503478";
     }
 
+    @Override
+    public String partTwoSolution() {
+        return "716241959649754";
+    }
+
     private static class Die {
         private int prev = 99;
         private int rolls;
@@ -62,6 +101,22 @@ public class Day21 extends Day {
             rolls++;
             prev = (prev + 1) % 100;
             return prev + 1;
+        }
+    }
+
+    private record Game(int p1Pos, int p1Score, int p2Pos, int p2Score, int whoseTurn) {
+        private boolean gameOver() {
+            return p1Score >= 21 || p2Score >= 21;
+        }
+
+        private Game turn(int dice) {
+            if (whoseTurn == 1) {
+                int pos = (this.p1Pos - 1 + dice) % 10 + 1;
+                return new Game(pos, p1Score + pos, p2Pos, p2Score, 2);
+            } else {
+                int pos = (this.p2Pos - 1 + dice) % 10 + 1;
+                return new Game(p1Pos, p1Score, pos, p2Score + pos, 1);
+            }
         }
     }
 }
