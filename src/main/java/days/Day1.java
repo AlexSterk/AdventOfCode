838de62ee1654e7cd4c5b05450e0c99b47a0d33c
Alexander Sterk <ajhsterk@gmail.com>
2022-12-01 12:13:48 +0100

    Day1

---
+++ src/main/java/days/Day1.java
@@ -0,0 +1,48 @@
+package days;
+
+import setup.Day;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day1 extends Day {
+
+    private List<List<Integer>> elfs;
+
+    @Override
+    public void processInput() {
+        elfs = Arrays.stream(input.split("(\n\r?){2}")).map(s -> Arrays.stream(s.split("\n\r?")).map(Integer::parseInt).toList()).toList();
+    }
+
+    @Override
+    public Object part1() {
+        // sum lists and get max
+        return elfs.stream().mapToInt(l -> l.stream().mapToInt(Integer::intValue).sum()).max().orElseThrow();
+    }
+
+    @Override
+    public String partOneSolution() {
+        return "70509";
+    }
+
+    @Override
+    public Object part2() {
+        // sum lists and sort in reverse and get first 3 summed
+        return elfs.stream().mapToInt(l -> l.stream().mapToInt(Integer::intValue).sum()).boxed().sorted((a, b) -> b - a).limit(3).mapToInt(Integer::intValue).sum();
+    }
+
+    @Override
+    public String partTwoSolution() {
+        return "208567";
+    }
+
+    @Override
+    public int getDay() {
+        return 1;
+    }
+
+    @Override
+    public boolean isTest() {
+        return false;
+    }
+}
