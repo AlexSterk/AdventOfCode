d6e7f5addd81e49766d694e552f7b31c9719fd66
Alexander Sterk <ajhsterk@gmail.com>
2022-12-06 09:57:57 +0100

    Day6 p2

--- src/main/java/days/Day6.java
+++ src/main/java/days/Day6.java
@@ -2,9 +2,7 @@ package days;
 
 import setup.Day;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.LinkedList;
 
 public class Day6 extends Day {
     @Override
@@ -14,29 +12,32 @@ public class Day6 extends Day {
 
     @Override
     public Object part1() {
-        Deque<String> queue = new ArrayDeque<>();
+        return getMarker(4);
+    }
 
+    private Integer getMarker(int targetSize) {
+        LinkedList<String> queue = new LinkedList<>();
         String[] chars = input.trim().split("");
         for (int i = 0; i < chars.length; i++) {
             String c = chars[i];
-            if (queue.contains(c)) {
-                queue.clear();
-                queue.add(c);
-            } else {
-                queue.add(c);
+            int j = queue.indexOf(c);
+            if (j != -1) {
+                for (int i1 = 0; i1 < j + 1; i1++) {
+                    queue.removeFirst();
+                }
             }
-            if (queue.size() == 4) {
+            queue.add(c);
+            if (queue.size() == targetSize) {
                 System.out.println(queue);
                 return i + 1;
             }
         }
-
         return null;
     }
 
     @Override
     public Object part2() {
-        return null;
+        return getMarker(14);
     }
 
     @Override
@@ -47,6 +48,11 @@ public class Day6 extends Day {
     @Override
     public String partOneSolution() {
         return "1140";
+    }
+
+    @Override
+    public String partTwoSolution() {
+        return "3495";
     }
 
     @Override
