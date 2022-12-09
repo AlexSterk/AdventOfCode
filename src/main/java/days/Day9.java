2f8e147d8024c4bcab682bea820fc55b4978e808
Alexander Sterk <ajhsterk@gmail.com>
2022-12-09 13:07:34 +0100

    Lots of refactoring day 9

--- src/main/java/days/Day9.java
+++ src/main/java/days/Day9.java
@@ -1,107 +1,84 @@
 package days;
 
 import setup.Day;
-import util.Grid;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Day9 extends Day {
+import static util.Grid.InfiniteGrid;
+import static util.Grid.Tile;
 
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class Day9 extends Day {
+    private InfiniteGrid grid;
 
     @Override
     public void processInput() {
-
+        grid = new InfiniteGrid(() -> null);
     }
 
     @Override
     public Object part1() {
-        Grid.InfiniteGrid<String> grid = new Grid.InfiniteGrid<>(() -> ".");
-        Grid.Tile<String> head, tail;
-        head = tail = grid.getTile(100, 100);
-        Set<Grid.Tile<String>> visited = new HashSet<>();
-        visited.add(tail);
+        Tile[] knots = new Tile[2];
+        Arrays.fill(knots, grid.getTile(100, 100));
+        Set<Tile> visited = new HashSet<>();
+        visited.add(knots[1]);
 
+        return runSimulation(knots, visited);
+    }
+
+    @Override
+    public Object part2() {
+        Tile[] knots = new Tile[10];
+        Arrays.fill(knots, grid.getTile(100, 100));
+        Set<Tile> visited = new HashSet<>();
+        visited.add(knots[9]);
+
+        return runSimulation(knots, visited);
+    }
+
+    private Object runSimulation(Tile[] knots, Set<Tile> visited) {
         for (String line : lines()) {
             String[] s = line.split(" ");
             String dir = s[0];
             int steps = Integer.parseInt(s[1]);
 
             for (int i = 0; i < steps; i++) {
-                head = switch (dir) {
-                    case "U" -> grid.getTile(head.x(), head.y() - 1);
-                    case "D" -> grid.getTile(head.x(), head.y() + 1);
-                    case "L" -> grid.getTile(head.x() - 1, head.y());
-                    case "R" -> grid.getTile(head.x() + 1, head.y());
-                    default -> head;
-                };
-
-                int dx = head.x() - tail.x();
-                int dy = head.y() - tail.y();
-
-                if (tail.manhattanDistance(head) <= 1) {
-                    // nothing
-                }
-
-                 else if (Math.abs(dx) == 1 && Math.abs(dy) == 1) {
-                    // nothing
-                } else {
-                    tail = grid.getTile(tail.x() + Integer.signum(dx), tail.y() + Integer.signum(dy));
-                }
-
-                visited.add(tail);
+                moveHead(knots, dir);
+                moveKnots(knots, visited);
             }
         }
 
         return visited.size();
     }
 
-    @Override
-    public Object part2() {
-        Grid.InfiniteGrid<String> grid = new Grid.InfiniteGrid<>(() -> ".");
-        Grid.Tile[] knots = new Grid.Tile[10];
-        Arrays.fill(knots, grid.getTile(100, 100));
-        Set<Grid.Tile<String>> visited = new HashSet<>();
-        visited.add(knots[9]);
+    private void moveKnots(Tile[] knots, Set<Tile> visited) {
+        for (int i = 1; i < knots.length; i++) {
+            var previous = knots[i - 1];
+            var current = knots[i];
 
-        for (String line : lines()) {
-            String[] s = line.split(" ");
-            String dir = s[0];
-            int steps = Integer.parseInt(s[1]);
+            int dx = previous.x() - current.x();
+            int dy = previous.y() - current.y();
 
-            for (int i = 0; i < steps; i++) {
-                knots[0] = switch (dir) {
-                    case "U" -> grid.getTile(knots[0].x(), knots[0].y() - 1);
-                    case "D" -> grid.getTile(knots[0].x(), knots[0].y() + 1);
-                    case "L" -> grid.getTile(knots[0].x() - 1, knots[0].y());
-                    case "R" -> grid.getTile(knots[0].x() + 1, knots[0].y());
-                    default -> knots[0];
-                };
+            if (current.manhattanDistance(previous) > 1 && (Math.abs(dx) != 1 || Math.abs(dy) != 1)) {
+                knots[i] = grid.getTile(current.x() + Integer.signum(dx), current.y() + Integer.signum(dy));
+            }
 
-                for (int j = 1; j < knots.length; j++) {
-                    var previous = knots[j - 1];
-                    var current = knots[j];
-
-                    int dx = previous.x() - current.x();
-                    int dy = previous.y() - current.y();
-
-                    if (current.manhattanDistance(previous) <= 1) {
-                        // nothing
-                    } else if (Math.abs(dx) == 1 && Math.abs(dy) == 1) {
-                        // nothing
-                    } else {
-                        knots[j] = grid.getTile(current.x() + Integer.signum(dx), current.y() + Integer.signum(dy));
-                    }
-
-                    if (j == knots.length - 1) {
-                        visited.add(knots[j]);
-                    }
-                }
+            if (i == knots.length - 1) {
+                visited.add(knots[i]);
             }
         }
+    }
 
-        return visited.size();
+    private void moveHead(Tile[] knots, String dir) {
+        knots[0] = switch (dir) {
+            case "U" -> grid.getTile(knots[0].x(), knots[0].y() - 1);
+            case "D" -> grid.getTile(knots[0].x(), knots[0].y() + 1);
+            case "L" -> grid.getTile(knots[0].x() - 1, knots[0].y());
+            case "R" -> grid.getTile(knots[0].x() + 1, knots[0].y());
+            default -> knots[0];
+        };
     }
 
     @Override
