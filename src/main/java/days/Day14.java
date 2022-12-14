f5251e1e59a8ef7d33c2e984a4da6a1861d69af8
Alexander Sterk <ajhsterk@gmail.com>
2022-12-14 22:04:38 +0100

    Day14 p2

--- src/main/java/days/Day14.java
+++ src/main/java/days/Day14.java
@@ -33,13 +33,13 @@ public class Day14 extends Day {
 
             rocks.add(pairs);
         }
-
-        cave = new Grid<>(maxX + 1, maxY + 1);
-        cave.fill('.');
     }
 
     @Override
     public Object part1() {
+        cave = new Grid<>(maxX + 1, maxY + 1);
+        cave.fill('.');
+
         for (List<Pair<Integer, Integer>> rock : rocks) {
             for (int i = 0; i < rock.size() - 1; i++) {
                 var start = rock.get(i);
@@ -58,13 +58,15 @@ public class Day14 extends Day {
             }
         }
 
+        cave.set(500, 0, '+');
+        int leftX = cave.getColumns().stream().dropWhile(list -> list.stream().allMatch(c -> c.data() == '.')).findFirst().get().get(0).x();
+        cave = cave.subgrid(leftX - 1, maxX, 0, maxY);
 
         System.out.printf("Before simulation%n%s%n%n", cave);
 
-        cave.set(500, 0, '+');
-        var source = cave.getTile(500, 0);
+        var source = cave.getAll().stream().filter(c -> c.data() == '+').findFirst().get();
         var sand = source;
-        int rested  = 0;
+        int rested = 0;
 
 
         while (true) {
@@ -100,7 +102,73 @@ public class Day14 extends Day {
 
     @Override
     public Object part2() {
-        return null;
+        cave = new Grid<>(maxX + 1 + 200, maxY + 1 + 2);
+        cave.fill('.');
+
+        for (List<Pair<Integer, Integer>> rock : rocks) {
+            for (int i = 0; i < rock.size() - 1; i++) {
+                var start = rock.get(i);
+                var end = rock.get(i + 1);
+
+                var startX = Math.min(start.a(), end.a());
+                var endX = Math.max(start.a(), end.a());
+                var startY = Math.min(start.b(), end.b());
+                var endY = Math.max(start.b(), end.b());
+
+                for (int x = startX; x <= endX; x++) {
+                    for (int y = startY; y <= endY; y++) {
+                        cave.set(x, y, '#');
+                    }
+                }
+            }
+        }
+        cave.getRow(maxY + 2).forEach(t -> cave.set(t, '#'));
+
+        cave.set(500, 0, '+');
+//        int leftX = cave.getColumns().stream().dropWhile(list -> list.stream().allMatch(c -> c.data() == '.')).findFirst().get().get(0).x();
+//        cave = cave.subgrid(leftX - 1, maxX, 0, maxY);
+
+        System.out.printf("Before simulation%n%s%n%n", cave);
+
+        var source = cave.getAll().stream().filter(c -> c.data() == '+').findFirst().get();
+        var sand = source;
+        int rested = 0;
+
+
+        while (true) {
+            if (sand.data() == 'o') {
+                break;
+            }
+
+
+            if (sand.down() == null) {
+                break;
+            }
+
+            if (sand.down().data() == '.') {
+                sand = sand.down();
+                continue;
+            }
+
+            if (sand.down().left().data() == '.') {
+                sand = sand.down().left();
+                continue;
+            }
+
+            if (sand.down().right().data() == '.') {
+                sand = sand.down().right();
+                continue;
+            }
+
+            cave.set(sand, 'o');
+            sand = cave.getTile(source.x() , source.y());
+            rested++;
+        }
+
+        System.out.printf("After simulation%n%s%n%n", cave);
+
+
+        return rested;
     }
 
     @Override
@@ -116,5 +184,10 @@ public class Day14 extends Day {
     @Override
     public String partOneSolution() {
         return "715";
+    }
+
+    @Override
+    public String partTwoSolution() {
+        return "25248";
     }
 }
