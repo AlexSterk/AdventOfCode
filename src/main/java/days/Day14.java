26c6035fec0917fc5b59e9a915e38dda021ee7c9
Alexander Sterk <ajhsterk@gmail.com>
2022-12-14 22:17:26 +0100

    clean up some code

--- src/main/java/days/Day14.java
+++ src/main/java/days/Day14.java
@@ -7,6 +7,7 @@ import util.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("DataFlowIssue")
 public class Day14 extends Day {
 
     int maxX, maxY;
@@ -38,42 +39,15 @@ public class Day14 extends Day {
     @Override
     public Object part1() {
         cave = new Grid<>(maxX + 1, maxY + 1);
-        cave.fill('.');
-
-        for (List<Pair<Integer, Integer>> rock : rocks) {
-            for (int i = 0; i < rock.size() - 1; i++) {
-                var start = rock.get(i);
-                var end = rock.get(i + 1);
-
-                var startX = Math.min(start.a(), end.a());
-                var endX = Math.max(start.a(), end.a());
-                var startY = Math.min(start.b(), end.b());
-                var endY = Math.max(start.b(), end.b());
-
-                for (int x = startX; x <= endX; x++) {
-                    for (int y = startY; y <= endY; y++) {
-                        cave.set(x, y, '#');
-                    }
-                }
-            }
-        }
+        initCave();
 
         cave.set(500, 0, '+');
-        int leftX = cave.getColumns().stream().dropWhile(list -> list.stream().allMatch(c -> c.data() == '.')).findFirst().get().get(0).x();
-        cave = cave.subgrid(leftX - 1, maxX, 0, maxY);
 
-        System.out.printf("Before simulation%n%s%n%n", cave);
-
-        var source = cave.getAll().stream().filter(c -> c.data() == '+').findFirst().get();
+        var source = cave.getTile(500, 0);
         var sand = source;
         int rested = 0;
 
-
-        while (true) {
-            if (sand.down() == null) {
-                break;
-            }
-
+        while (sand.down() != null) {
             if (sand.down().data() == '.') {
                 sand = sand.down();
                 continue;
@@ -94,15 +68,47 @@ public class Day14 extends Day {
             rested++;
         }
 
-        System.out.printf("After simulation%n%s%n%n", cave);
-
-
         return rested;
     }
 
     @Override
     public Object part2() {
         cave = new Grid<>(maxX + 1 + 200, maxY + 1 + 2);
+        initCave();
+        cave.getRow(maxY + 2).forEach(t -> cave.set(t, '#'));
+
+        cave.set(500, 0, '+');
+
+        var source = cave.getTile(500, 0);
+        var sand = source;
+        int rested = 0;
+
+        while (sand.data() != 'o') {
+            if (sand.down().data() == '.') {
+                sand = sand.down();
+                continue;
+            }
+
+            if (sand.down().left().data() == '.') {
+                sand = sand.down().left();
+                continue;
+            }
+
+            if (sand.down().right().data() == '.') {
+                sand = sand.down().right();
+                continue;
+            }
+
+            cave.set(sand, 'o');
+            sand = cave.getTile(source.x(), source.y());
+            rested++;
+        }
+
+
+        return rested;
+    }
+
+    private void initCave() {
         cave.fill('.');
 
         for (List<Pair<Integer, Integer>> rock : rocks) {
@@ -122,53 +128,6 @@ public class Day14 extends Day {
                 }
             }
         }
-        cave.getRow(maxY + 2).forEach(t -> cave.set(t, '#'));
-
-        cave.set(500, 0, '+');
-//        int leftX = cave.getColumns().stream().dropWhile(list -> list.stream().allMatch(c -> c.data() == '.')).findFirst().get().get(0).x();
-//        cave = cave.subgrid(leftX - 1, maxX, 0, maxY);
-
-        System.out.printf("Before simulation%n%s%n%n", cave);
-
-        var source = cave.getAll().stream().filter(c -> c.data() == '+').findFirst().get();
-        var sand = source;
-        int rested = 0;
-
-
-        while (true) {
-            if (sand.data() == 'o') {
-                break;
-            }
-
-
-            if (sand.down() == null) {
-                break;
-            }
-
-            if (sand.down().data() == '.') {
-                sand = sand.down();
-                continue;
-            }
-
-            if (sand.down().left().data() == '.') {
-                sand = sand.down().left();
-                continue;
-            }
-
-            if (sand.down().right().data() == '.') {
-                sand = sand.down().right();
-                continue;
-            }
-
-            cave.set(sand, 'o');
-            sand = cave.getTile(source.x() , source.y());
-            rested++;
-        }
-
-        System.out.printf("After simulation%n%s%n%n", cave);
-
-
-        return rested;
     }
 
     @Override
