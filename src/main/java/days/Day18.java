a2aa9b3811d110c4e65ff659bc093d7e299dba0d
Alexander Sterk <ajhsterk@gmail.com>
2022-12-18 15:26:48 +0100

    remove useless stuff

--- src/main/java/days/Day18.java
+++ src/main/java/days/Day18.java
@@ -1,38 +1,23 @@
 package days;
 
 import setup.Day;
-import util.Graph;
 
 import java.util.*;
 
 public class Day18 extends Day {
 
     private List<Point> points;
-    private Graph<Point> graph;
-
     @Override
     public void processInput() {
         points = lines().stream().map(s -> {
             var split = s.split(",");
             return new Point(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]));
         }).toList();
-
-        graph = new Graph<>();
-
-        points.forEach(graph::addNode);
-        for (Point p1 : points) {
-            for (Point p2 : points) {
-                if (p1 == p2) continue;
-                if (p1.manHattanDistance(p2) == 1) {
-                    graph.addEdge(p1, p2, 1);
-                }
-            }
-        }
     }
 
     @Override
     public Object part1() {
-        return points.stream().map(graph::getNeighbours).mapToInt(n -> 6 - n.size()).sum();
+        return points.stream().map(p -> p.neighbours().stream().filter(points::contains).toList()).mapToInt(n -> 6 - n.size()).sum();
     }
 
     @Override
@@ -102,10 +87,6 @@ public class Day18 extends Day {
     }
 
     private record Point(int x, int y, int z) {
-        private int manHattanDistance(Point other) {
-            return Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(z - other.z);
-        }
-
         private Set<Point> neighbours() {
             return Set.of(
                     new Point(x + 1, y, z),
