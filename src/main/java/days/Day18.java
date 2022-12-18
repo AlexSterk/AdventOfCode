304b7be9491797eda9b23732814eb844616b3a44
Alexander Sterk <ajhsterk@gmail.com>
2022-12-18 14:59:38 +0100

    Day18 p2

--- src/main/java/days/Day18.java
+++ src/main/java/days/Day18.java
@@ -3,7 +3,7 @@ package days;
 import setup.Day;
 import util.Graph;
 
-import java.util.List;
+import java.util.*;
 
 public class Day18 extends Day {
 
@@ -37,7 +37,48 @@ public class Day18 extends Day {
 
     @Override
     public Object part2() {
-        return null;
+        int minX, minY, minZ;
+        int maxX, maxY, maxZ;
+
+        minX = minY = minZ = Integer.MAX_VALUE;
+        maxX = maxY = maxZ = Integer.MIN_VALUE;
+
+        for (Point p : points) {
+            if (p.x < minX) minX = p.x;
+            if (p.y < minY) minY = p.y;
+            if (p.z < minZ) minZ = p.z;
+            if (p.x > maxX) maxX = p.x;
+            if (p.y > maxY) maxY = p.y;
+            if (p.z > maxZ) maxZ = p.z;
+        }
+
+        minX -= 1;
+        minY -= 1;
+        minZ -= 1;
+
+        maxX += 1;
+        maxY += 1;
+        maxZ += 1;
+
+        Point cur = new Point(minX, minY, minZ);
+        int area = 0;
+
+        Queue<Point> toVisit = new ArrayDeque<>();
+        toVisit.offer(cur);
+        Set<Point> visited = new HashSet<>();
+
+        while (!toVisit.isEmpty()) {
+            cur = toVisit.poll();
+            if (visited.contains(cur)) continue;
+            visited.add(cur);
+            for (Point n : cur.neighbours()) {
+                if (visited.contains(n)) continue;
+                if (points.contains(n)) area += 1;
+                else if (n.x >= minX && n.x <= maxX && n.y >= minY && n.y <= maxY && n.z >= minZ && n.z <= maxZ) toVisit.offer(n);
+            }
+        }
+
+        return area;
     }
 
     @Override
@@ -55,9 +96,25 @@ public class Day18 extends Day {
         return "4604";
     }
 
+    @Override
+    public String partTwoSolution() {
+        return "2604";
+    }
+
     private record Point(int x, int y, int z) {
         private int manHattanDistance(Point other) {
             return Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(z - other.z);
+        }
+
+        private Set<Point> neighbours() {
+            return Set.of(
+                    new Point(x + 1, y, z),
+                    new Point(x - 1, y, z),
+                    new Point(x, y + 1, z),
+                    new Point(x, y - 1, z),
+                    new Point(x, y, z + 1),
+                    new Point(x, y, z - 1)
+            );
         }
     }
 }
