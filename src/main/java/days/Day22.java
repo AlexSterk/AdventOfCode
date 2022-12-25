73d49249d807b8ee4f2cd42d19156a091ce57d31
Alexander Sterk <ajhsterk@gmail.com>
2022-12-25 15:29:10 +0100

    Day22 stuck with part 2

--- src/main/java/days/Day22.java
+++ src/main/java/days/Day22.java
@@ -4,7 +4,6 @@ import setup.Day;
 import util.Grid;
 import util.Grid.Tile;
 import util.Line.Point;
-import util.Pair;
 
 import java.util.*;
 import java.util.regex.MatchResult;
@@ -28,7 +27,7 @@ public class Day22 extends Day {
     @Override
     public Object part1() {
         var current = grid.getRow(0).stream().filter(s -> s.data().equals(".")).findFirst().get();
-        Direction dir = Direction.RIGHT;
+        Direction dir = Direction.EAST;
 
         System.out.println(grid);
 
@@ -41,21 +40,21 @@ public class Day22 extends Day {
                 int steps = Integer.parseInt(instruction);
                 for (int i = 0; i < steps; i++) {
                     var next = switch (dir) {
-                        case UP -> current.up();
-                        case DOWN -> current.down();
-                        case LEFT -> current.left();
-                        case RIGHT -> current.right();
+                        case NORTH -> current.up();
+                        case SOUTH -> current.down();
+                        case WEST -> current.left();
+                        case EAST -> current.right();
                     };
                     if (next == null || next.data().equals(" ")) {
                         // wrap around and search for non-space
                         next = switch (dir) {
-                            case UP ->
+                            case NORTH ->
                                     grid.getColumn(current.x()).stream().filter(Objects::nonNull).filter(s -> !s.data().equals(" ")).reduce((a, b) -> b).get();
-                            case DOWN ->
+                            case SOUTH ->
                                     grid.getColumn(current.x()).stream().filter(Objects::nonNull).filter(s -> !s.data().equals(" ")).reduce((a, b) -> a).get();
-                            case LEFT ->
+                            case WEST ->
                                     grid.getRow(current.y()).stream().filter(Objects::nonNull).filter(s -> !s.data().equals(" ")).reduce((a, b) -> b).get();
-                            case RIGHT ->
+                            case EAST ->
                                     grid.getRow(current.y()).stream().filter(Objects::nonNull).filter(s -> !s.data().equals(" ")).reduce((a, b) -> a).get();
                         };
                     }
@@ -68,66 +67,21 @@ public class Day22 extends Day {
         }
 
         return 1000 * (current.y() + 1) + 4 * (current.x() + 1) + switch (dir) {
-            case RIGHT -> 0;
-            case DOWN -> 1;
-            case LEFT -> 2;
-            case UP -> 3;
+            case EAST -> 0;
+            case SOUTH -> 1;
+            case WEST -> 2;
+            case NORTH -> 3;
         };
     }
 
     @Override
     public Object part2() {
-        var sides = findCubeSubgrids();
-        var map = getAdjacencyMap(sides, getSize());
-
-        var currentSide = sides.values().stream().filter(s -> s.name.equals("A")).findFirst().get();
-        var currentTile = currentSide.grid.getRow(0).stream().filter(s -> s.data().equals(".")).findFirst().get();
-        var currentDir = Direction.RIGHT;
-
-        for (String instruction : instructions) {
-            if (instruction.equals("L")) {
-                currentDir = currentDir.turnLeft();
-            } else if (instruction.equals("R")) {
-                currentDir = currentDir.turnRight();
-            } else {
-                int steps = Integer.parseInt(instruction);
-                for (int i = 0; i < steps; i++) {
-                    var next = switch (currentDir) {
-                        case UP -> currentTile.up();
-                        case DOWN -> currentTile.down();
-                        case LEFT -> currentTile.left();
-                        case RIGHT -> currentTile.right();
-                    };
-                    var nextSide = currentSide;
-                    if (next == null) {
-                        // move to next side
-                        nextSide = map.get(new Pair<>(currentSide, currentDir));
-                        next = nextSide.grid.getTile(switch (currentDir) {
-                            case UP, DOWN -> currentTile.x();
-                            case LEFT -> currentSide.grid.width - 1;
-                            case RIGHT -> 0;
-                        }, switch (currentDir) {
-                            case UP -> currentSide.grid.height - 1;
-                            case DOWN -> 0;
-                            case LEFT, RIGHT -> currentTile.y();
-                        });
-                    }
-                    if (next.data().equals("#")) {
-                        break;
-                    }
-                    currentTile = next;
-                    currentSide = nextSide;
-                }
-            }
-        }
-
-        System.out.println(currentSide);
 
         return null;
     }
 
-    private Map<Point, CubeSide> findCubeSubgrids() {
-        Map<Point, CubeSide> sides = new HashMap<>();
+    private TreeMap<Point, CubeFace> findFaces() {
+        TreeMap<Point, CubeFace> sides = new TreeMap<>(Comparator.comparingInt(Point::y).thenComparingInt(Point::x));
 
         int size = getSize();
         int cubes = 0;
@@ -136,8 +90,8 @@ public class Day22 extends Day {
                 var possibleCube = grid.subgrid(x, x + size - 1, y, y + size - 1);
                 List<Tile<String>> all = possibleCube.getAll();
                 if (all.size() == size * size && all.stream().noneMatch(s -> s == null || s.data().equals(" "))) {
-                    Point key = new Point(x, y);
-                    sides.put(key, new CubeSide(Character.toString('A' + (cubes++)), possibleCube));
+                    Point key = new Point(x / size, y / size);
+                    sides.put(key, new CubeFace(Character.toString('A' + (cubes++)), possibleCube, key));
                 }
             }
         }
@@ -149,77 +103,6 @@ public class Day22 extends Day {
         return isTest() ? 4 : 50;
     }
 
-    private CubeSide getAdjacent(Map<Pair<CubeSide, Direction>, CubeSide> adjacency, CubeSide side, Direction dir) {
-        if (adjacency.containsKey(new Pair<>(side, dir))) {
-            return adjacency.get(new Pair<>(side, dir));
-        }
-        var leftTurn = dir.turnLeft();
-        CubeSide adjacent = getAdjacent(adjacency, side, leftTurn);
-        CubeSide adjacent1 = getAdjacent(adjacency, adjacent, dir);
-        adjacency.put(new Pair<>(side, dir), new CubeSide(adjacent1.name + "_rot", adjacent1.grid.rotate()));
-        return adjacent1;
-    }
-
-    private Map<Pair<CubeSide, Direction>, CubeSide> getAdjacencyMap(Map<Point, CubeSide> sides, int size) {
-        Map<Pair<CubeSide, Direction>, CubeSide> adjacencyMap = new HashMap<>();
-        for (var entry : sides.entrySet()) {
-            Point key = entry.getKey();
-            CubeSide value = entry.getValue();
-            for (Direction dir : Direction.values()) {
-                Point next = switch (dir) {
-                    case UP -> new Point(key.x(), key.y() - size);
-                    case DOWN -> new Point(key.x(), key.y() + size);
-                    case LEFT -> new Point(key.x() - size, key.y());
-                    case RIGHT -> new Point(key.x() + size, key.y());
-                };
-                if (sides.containsKey(next)) {
-                    adjacencyMap.put(new Pair<>(value, dir), sides.get(next));
-                }
-            }
-        }
-
-        while (true) {
-            var temp = new HashMap<>(adjacencyMap);
-            for (CubeSide side : new HashSet<>(temp.values())) {
-                for (Direction direction : Direction.values()) {
-                    if (adjacencyMap.containsKey(new Pair<>(side, direction))) {
-                        continue;
-                    }
-                    // go thrice in the opposite direction
-                    var opposite = direction.opposite();
-                    try {
-                        var next = adjacencyMap.get(new Pair<>(side, opposite));
-                        next = adjacencyMap.get(new Pair<>(next, opposite));
-                        next = adjacencyMap.get(new Pair<>(next, opposite));
-                        adjacencyMap.put(new Pair<>(side, direction), next);
-                        if (next != null) continue;
-                    } catch (NullPointerException e) {
-                        // ignore
-                    }
-                    // turn once, then go in the desired direction
-                    var turn = direction.turnLeft();
-                    try {
-                        var next = adjacencyMap.get(new Pair<>(side, turn));
-                        var nextNext = adjacencyMap.get(new Pair<>(next, direction));
-                        CubeSide rotated = new CubeSide(nextNext.name + "_rot", nextNext.grid.rotate());
-                        adjacencyMap.put(new Pair<>(side, direction), rotated);
-                        adjacencyMap.put(new Pair<>(rotated, turn.opposite()), side);
-                    } catch (NullPointerException ignored) {
-
-                    }
-                }
-            }
-            adjacencyMap.values().removeIf(Objects::isNull);
-            if (temp.size() == adjacencyMap.size()) {
-                break;
-            }
-        }
-
-
-
-        return adjacencyMap;
-    }
-
     @Override
     public int getDay() {
         return 22;
@@ -227,7 +110,7 @@ public class Day22 extends Day {
 
     @Override
     public boolean isTest() {
-        return true;
+        return false;
     }
 
     @Override
@@ -236,44 +119,42 @@ public class Day22 extends Day {
     }
 
     private enum Direction {
-        UP, DOWN, LEFT, RIGHT;
+        EAST(1, 0),
+        SOUTH(0, 1),
+        WEST(-1, 0),
+        NORTH(0, -1);
+        public final int dx, dy;
+        private static final List<Direction> values = List.of(Direction.values());
+
+        Direction(int dx, int dy) {
+            this.dx = dx;
+            this.dy = dy;
+        }
 
         public Direction turnLeft() {
-            return switch (this) {
-                case UP -> LEFT;
-                case DOWN -> RIGHT;
-                case LEFT -> DOWN;
-                case RIGHT -> UP;
-            };
+            return values.get((values.indexOf(this) + 3) % 4);
         }
 
         public Direction turnRight() {
-            return switch (this) {
-                case UP -> RIGHT;
-                case DOWN -> LEFT;
-                case LEFT -> UP;
-                case RIGHT -> DOWN;
-            };
+            return values.get((values.indexOf(this) + 1) % 4);
         }
 
         public Direction opposite() {
-            return switch (this) {
-                case UP -> DOWN;
-                case DOWN -> UP;
-                case LEFT -> RIGHT;
-                case RIGHT -> LEFT;
-            };
+            return values.get((values.indexOf(this) + 2) % 4);
         }
 
     }
 
-    private record CubeSide(String name, Grid<String> grid) {
+    private record CubeFace(String name, Grid<String> grid, Point position) {
         @Override
         public String toString() {
             return "CubeSide{" +
                     "name='" + name + '\'' +
+                    ", position=" + position +
                     ", grid=\n" + grid +
                     "\n}";
         }
     }
+
+
 }
