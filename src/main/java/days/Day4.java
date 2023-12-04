5bf10d1e5cfdf8005fe0c7ff2ce708de1eb415ab
Alexander Sterk <ajhsterk@gmail.com>
2023-12-04 13:52:55 +0100

    d4p2

--- src/main/java/days/Day4.java
+++ src/main/java/days/Day4.java
@@ -2,17 +2,17 @@ package days;
 
 import setup.Day;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day4 extends Day {
     private List<Card> cards;
+    private Map<Integer, Card> cardsMap;
 
     @Override
     public void processInput() {
         cards = this.lines().stream().map(Card::parse).collect(Collectors.toList());
+        cardsMap = cards.stream().collect(Collectors.toMap(Card::id, c -> c));
     }
 
     @Override
@@ -22,7 +22,20 @@ public class Day4 extends Day {
 
     @Override
     public Object part2() {
-        return null;
+        new ArrayList<>(cards).forEach(this::processWins);
+
+        return cards.size();
+    }
+
+    private void processWins(Card card) {
+        var wins = card.wins();
+        var id = card.id();
+
+        for (int i = 1; i <= wins; i++) {
+            var nCard = cardsMap.get(id + i);
+            cards.add(nCard);
+            this.processWins(nCard);
+        }
     }
 
     @Override
@@ -49,8 +62,23 @@ public class Day4 extends Day {
         }
 
         public long value() {
-            var intersection = winning.stream().filter(numbers::contains).collect(Collectors.toSet());
-            return (long) Math.floor(Math.pow(2, intersection.size() - 1));
+            var wins = this.wins();
+            return (long) Math.floor(Math.pow(2, wins - 1));
         }
+
+        public int wins() {
+            var intersection = winning.stream().filter(numbers::contains).collect(Collectors.toSet());
+            return intersection.size();
+        }
+    }
+
+    @Override
+    public String partOneSolution() {
+        return "20407";
+    }
+
+    @Override
+    public String partTwoSolution() {
+        return "23806951";
     }
 }
