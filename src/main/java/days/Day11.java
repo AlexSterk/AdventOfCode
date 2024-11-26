9cc928f866390e6a88f40a9f0089979e43e7ffb5
Alexander Sterk <ajhsterk@gmail.com>
2024-11-26 15:56:53 +0100

    day11 p1 working

--- src/main/java/days/Day11.java
+++ src/main/java/days/Day11.java
@@ -20,66 +20,50 @@ public class Day11 extends Day {
 
     @Override
     public Object part1() {
-        List<Integer> emptyRowsIndexes = new ArrayList<>();
-        List<Integer> emptyColumnsIndexes = new ArrayList<>();
+        var emptyRows = new ArrayList<Integer>();
+        var emptyCols = new ArrayList<Integer>();
 
-        for (int i = 0; i < image.getRows().size(); i++) {
+        for (int i = 0; i < image.height; i++) {
             if (image.getRow(i).stream().allMatch(c -> c.data().equals("."))) {
-                emptyRowsIndexes.add(i);
+                emptyRows.add(i);
             }
         }
 
-        for (int i = 0; i < image.getColumns().size(); i++) {
+        for (int i = 0; i < image.width; i++) {
             if (image.getColumn(i).stream().allMatch(c -> c.data().equals("."))) {
-                emptyColumnsIndexes.add(i);
+                emptyCols.add(i);
             }
         }
 
-        System.out.println(emptyRowsIndexes);
-        System.out.println(emptyColumnsIndexes);
-
-        StringBuilder sb = new StringBuilder();
-
-        int nWidth = image.width + emptyColumnsIndexes.size();
-
-        System.out.println("Creating new image");
-
-        for (int y = 0; y < image.getRows().size(); y++) {
-            if (emptyRowsIndexes.contains(y)) {
-                sb.append(".".repeat(nWidth));
-                sb.append("\n");
-            }
-
-            for (int x = 0; x < image.getColumns().size(); x++) {
-                if (emptyColumnsIndexes.contains(x)) {
-                    sb.append(".");
-                }
-
-                sb.append(image.getTile(x, y).data());
-            }
-
-            sb.append("\n");
-        }
-
-        System.out.println("Parsing new image");
-
-        image = Grid.parseGrid(sb.toString());
-        Grid<S> grid = image.map(S::new);
-        var graph = Grid.gridToGraph(grid);
-        var galaxies = grid.getAll().stream().filter(c -> c.data().c.equals("#")).toList();
-
-        System.out.println("Calculating distances for " + galaxies.size() + " galaxies");
+        var galaxies = image.getAll().stream().filter(c -> c.data().equals("#")).toList();
 
         Map<String, Integer> distances = new HashMap<>();
 
-        // cartesian product of galaxies
         for (int i = 0; i < galaxies.size(); i++) {
-            for (int j = i + 1; j < galaxies.size(); j++) {
-                String s = i + "," + j;
-                System.out.println("Calculating distance for " + s);
-                if (distances.containsKey(s)) continue;
-                var distance = graph.getDistance(galaxies.get(i), galaxies.get(j));
-                distances.put(s, distance);
+            for (int j = i+1; j < galaxies.size(); j++) {
+                var a = galaxies.get(i);
+                var b = galaxies.get(j);
+
+                if (a.equals(b) ) {
+                    continue;
+                }
+
+                var key  = "%d,%d".formatted(i,j);
+                var distance = Math.abs(a.x() - b.x()) + Math.abs(a.y() - b.y());
+
+                // add 1 for every empty row or column between the two galaxies
+                for (int row : emptyRows) {
+                    if (a.y() < row && b.y() > row || a.y() > row && b.y() < row) {
+                        distance += 1;
+                    }
+                }
+                for (int col : emptyCols) {
+                    if (a.x() < col && b.x() > col || a.x() > col && b.x() < col) {
+                        distance += 1;
+                    }
+                }
+
+                distances.put(key, distance);
             }
         }
 
@@ -106,15 +90,8 @@ public class Day11 extends Day {
         return false;
     }
 
-    private record S(String c) implements Grid.Weighted {
-        @Override
-        public Integer getWeight() {
-            return 1;
-        }
-
-        @Override
-        public String toString() {
-            return c;
-        }
+    @Override
+    public String partOneSolution() {
+        return "9214785";
     }
 }
