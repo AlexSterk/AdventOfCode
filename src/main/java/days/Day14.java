d0b7d6c9f65d69c77354bdeb89ead1ac9786d4b4
Alexander Sterk <ajhsterk@gmail.com>
2024-11-27 23:54:56 +0100

    d14 p2

--- src/main/java/days/Day14.java
+++ src/main/java/days/Day14.java
@@ -1,8 +1,10 @@
 package days;
 
 import setup.Day;
+import util.Direction;
 import util.Grid;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day14 extends Day {
@@ -15,8 +17,7 @@ public class Day14 extends Day {
 
     @Override
     public Object part1() {
-
-        rollGridNorth();
+        rollGrid(Direction.N);
 
         return calculateLoad();
     }
@@ -27,12 +28,39 @@ public class Day14 extends Day {
 
     @Override
     public Object part2() {
+        List<Grid<String>> grids = new ArrayList<>();
+
+        for (long i = 0; i < 1000000000L; i++) {
+            grid = grid.copy();
+            cycle();
+            if (grids.contains(grid)) {
+                var cycleLength = grids.size() - grids.indexOf(grid);
+                var remaining = 1000000000L - i - 1;
+                var cycleIndex = (int) (remaining % cycleLength);
+                grid = grids.get(grids.indexOf(grid) + cycleIndex);
+                return calculateLoad();
+            }
+            grids.add(grid);
+        }
+
         return null;
+    }
+
+    private void cycle() {
+        rollGrid(Direction.N);
+        rollGrid(Direction.W);
+        rollGrid(Direction.S);
+        rollGrid(Direction.E);
     }
 
     @Override
     public int getDay() {
         return 14;
+    }
+
+    @Override
+    public boolean resetForPartTwo() {
+        return true;
     }
 
     @Override
@@ -45,24 +73,22 @@ public class Day14 extends Day {
         return "109939";
     }
 
-    private void rollGridNorth() {
-        while (!isStable()) {
-            for (Grid.Tile<String> movable : movables()) {
-                var up = movable.up();
-                grid.set(up, "O");
+    private void rollGrid(Direction dir) {
+        var movables = movables(dir);
+        while (!movables.isEmpty()) {
+            for (Grid.Tile<String> movable : movables) {
+                var next = grid.getTile(movable.x() + dir.dx, movable.y() + dir.dy);
+                grid.set(next, "O");
                 grid.set(movable, ".");
             }
+            movables = movables(dir);
         }
     }
 
-    private List<Grid.Tile<String>> movables() {
+    private List<Grid.Tile<String>> movables(Direction dir) {
         return grid.getAll().stream().filter(t -> t.data().equals("O")).filter(t -> {
-            var up = t.up();
-            return up != null && up.data().equals(".");
+            var next = grid.getTile(t.x() + dir.dx, t.y() + dir.dy);
+            return next != null && next.data().equals(".");
         }).toList();
-    }
-
-    private boolean isStable() {
-        return movables().isEmpty();
     }
 }
