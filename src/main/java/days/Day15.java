a4c75786f4924454cccee1be9fd1b43f47d68795
Alexander Sterk <ajhsterk@gmail.com>
2024-11-28 00:29:38 +0100

    d15 p2

--- src/main/java/days/Day15.java
+++ src/main/java/days/Day15.java
@@ -2,26 +2,57 @@ package days;
 
 import setup.Day;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Day15 extends Day {
 
-    private List<String> steps;
+    private List<Step> steps;
 
     @Override
     public void processInput() {
-        steps = Arrays.stream(input.trim().split(",")).toList();
+        steps = Arrays.stream(input.trim().split(","))
+                .map(Step::new)
+                .toList();
     }
 
     @Override
     public Object part1() {
-        return steps.stream().mapToLong(this::hash).sum();
+        return steps.stream().map(Step::toString).mapToLong(this::hash).sum();
     }
 
     @Override
     public Object part2() {
-        return null;
+        var map = new HashMap<Integer, LinkedHashMap<String, Integer>>();
+
+        for (Step step : steps) {
+            int hash = hash(step.label);
+            var box = map.computeIfAbsent(hash, k -> new LinkedHashMap<>());
+
+            if (step.operation == '-') {
+                box.remove(step.label);
+            }
+
+            if (step.operation == '=') {
+                box.put(step.label, step.focalLength);
+            }
+        }
+
+        long sum = 0;
+
+        for (int i = 0; i < 255; i++) {
+            var box = map.get(i);
+
+            if (box == null || box.isEmpty()) continue;
+
+            int j = i + 1;
+            int k = 1;
+            for (Integer f : box.sequencedValues()) {
+                sum += (long) j * k * f;
+                k++;
+            }
+        }
+
+        return sum;
     }
 
     @Override
@@ -45,5 +76,23 @@ public class Day15 extends Day {
         }
 
         return cur;
+    }
+
+    private static class Step {
+        public final String label;
+        public final char operation;
+        public final Integer focalLength;
+
+        public Step(String s) {
+            var split = s.splitWithDelimiters("[=-]", 0);
+            label = split[0];
+            operation = split[1].charAt(0);
+            focalLength = split.length > 2 ? Integer.parseInt(split[2]) : null;
+        }
+
+        @Override
+        public String toString() {
+            return "%s%c%s".formatted(label, operation, focalLength == null ? "" : focalLength);
+        }
     }
 }
