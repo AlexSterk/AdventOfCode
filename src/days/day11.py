54f2e1f4c443f5f1205a0e313fea4fba1ef1857f
Alexander Sterk <ajhsterk@gmail.com>
2024-12-12 00:25:18 +0100

    make p1 and p2 the same

--- src/days/day11.py
+++ src/days/day11.py
@@ -16,21 +16,6 @@ def process_stone(stone):
         return left, right
     return stone * 2024
 
-def blink(stones):
-    cont_next = False
-    for i, stone in enumerate(stones):
-        if cont_next:
-            cont_next = False
-            continue
-        output = process_stone(stone)
-        if isinstance(output, tuple):
-            stones[i] = output[0]
-            stones.insert(i+1, output[1])
-            cont_next = True
-        else:
-            stones[i] = output
-    return stones
-
 
 class Day11(Day):
     @property
@@ -39,35 +24,34 @@ class Day11(Day):
 
     @solution("182081")
     def part1(self, n=25) -> object:
-        stones = [int(x) for x in self.raw_input.split()]
-
-        for i in range(n):
-            stones = blink(stones)
-
-        return len(stones)
-
-    @solution("216318908621637")
-    def part2(self, n=75) -> object:
         _stones = [int(x) for x in self.raw_input.split()]
         stones = {stone: 0 for stone in _stones}
         for stone in _stones:
             stones[stone] += 1
 
         for i in range(n):
-            temp = {}
-            for stone, count in list(stones.items()):
-                p = process_stone(stone)
-                if isinstance(p, tuple):
-                    l, r = p
-                    c_l = temp.get(l) or 0
-                    temp[l] = c_l + count
-                    c_r = temp.get(r) or 0
-                    temp[r] = c_r + count
-                else:
-                    temp[p] = (temp.get(p) or 0) + count
-            stones = temp
+            stones = self.blink(stones)
 
         return sum(stones.values())
+
+    def blink(self, stones):
+        temp = {}
+        for stone, count in list(stones.items()):
+            p = process_stone(stone)
+            if isinstance(p, tuple):
+                l, r = p
+                c_l = temp.get(l) or 0
+                temp[l] = c_l + count
+                c_r = temp.get(r) or 0
+                temp[r] = c_r + count
+            else:
+                temp[p] = (temp.get(p) or 0) + count
+        stones = temp
+        return stones
+
+    @solution("216318908621637")
+    def part2(self, n=75) -> object:
+        return self.part1(n, skip_test=True)
 
 
 # Day11("test2").run()
