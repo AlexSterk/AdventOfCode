1304228d0ddd245d201f0dd2cd0732c2ed993c22
Alexander Sterk <ajhsterk@gmail.com>
2024-12-14 23:26:48 +0100

    d23 p2

--- src/days/day23.py
+++ src/days/day23.py
@@ -2,7 +2,17 @@ from src.setup.day import Day
 from src.util.solution import solution
 
 ''' to optimize:
-cpy 0 a
+3: cpy 0 a             a = 0                  a = 0             a = 0            a = d * b
+4: cpy b c             c = b                  while d != 0:     while d != 0:    d = 0
+5: inc a               a += 1                     c = b             a += b       c = 0
+6: dec c               c -= 1                     a += c            d -= 1    
+7: jnz c -2            if c != 0: goto 5          d -= 1        c = 0        
+8: dec d               d -= 1                     c = 0
+9: jnz d -5            if d != 0: goto 4
+
+does the following:
+a = d * c
+
 cpy b c
 inc a
 dec c
@@ -10,8 +20,13 @@ jnz c -2
 dec d
 jnz d -5
 
-does the following:
-a = d * c
+translates to:
+mul b d a
+cpy 0 c
+cpy 0 c
+cpy 0 c
+cpy 0 c
+cpy 0 d
 '''
 
 def run_cpu(instructions, registers):
@@ -25,13 +40,16 @@ def run_cpu(instructions, registers):
 
     while pc < len(instructions):
         ins, *args = instructions[pc]
-        print(pc, ins, args)
+        # print(pc, ins, args) # find the loop
         if ins == "cpy":
             registers[args[1]] = val(args[0])
         elif ins == "inc":
             registers[args[0]] += 1
         elif ins == "dec":
             registers[args[0]] -= 1
+        elif ins == "mul":
+            x, y, z = args
+            registers[z] = val(x) * val(y)
         elif ins == "jnz":
             if val(args[0]) != 0:
                 pc += val(args[1])
