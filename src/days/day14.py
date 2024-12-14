4cc1ff581ec9c9adb15f6472567bc98f1042fb5c
Alexander Sterk <ajhsterk@gmail.com>
2024-12-14 10:55:45 +0100

    use simple heuristic to do p2 instead

--- src/days/day14.py
+++ src/days/day14.py
@@ -1,8 +1,4 @@
-import io
-import math
 import re
-import numpy as np
-import imageio
 
 from src.setup.day import Day
 from src.util.solution import solution
@@ -70,36 +66,14 @@ class Day14(Day):
         def get_state():
             return tuple((robot["x"], robot["y"]) for robot in robots)
 
-        min_size = (0, math.inf)
-
-        def write_image():
-            nonlocal min_size
-            img = np.zeros((h, w, 3), dtype=np.uint8)
-            for robot in robots:
-                img[robot["y"], robot["x"]] = [255, 255, 255]
-            buffer = io.BytesIO()
-            imageio.imwrite(buffer, img, format="png")
-            size = buffer.tell()
-            if size < min_size[1]:
-                min_size = (i, size)
-            buffer.close()
-
-        seen = set()
-        seen.add(get_state())
         i = 0
-
         while True:
             i += 1
             for robot in robots:
                 robot["x"] = (robot["x"] + robot["dx"]) % w
                 robot["y"] = (robot["y"] + robot["dy"]) % h
-
-            state = get_state()
-            if state in seen:
-                break
-            seen.add(state)
-            write_image()
-        return min_size[0]
+            if len(robots) == len(set(get_state())):
+                return i
 
 
 # Day14("test").run()
