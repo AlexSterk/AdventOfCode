b39064ab1818f7ccf714633d97b43022a0bca328
Alexander Sterk <ajhsterk@gmail.com>
2024-12-14 10:46:33 +0100

    d14 p2

--- src/days/day14.py
+++ src/days/day14.py
@@ -1,7 +1,12 @@
+import io
+import math
 import re
+import numpy as np
+import imageio
 
 from src.setup.day import Day
 from src.util.solution import solution
+
 
 class Day14(Day):
     @property
@@ -12,7 +17,7 @@ class Day14(Day):
     def part1(self) -> object:
         robots = []
         for line in self.input:
-            x,y,dx,dy = re.findall(r'-?\d+', line)
+            x, y, dx, dy = re.findall(r'-?\d+', line)
             robots.append({
                 "x": int(x),
                 "y": int(y),
@@ -50,7 +55,53 @@ class Day14(Day):
 
     @solution("")
     def part2(self) -> object:
-        return None
+        robots = []
+        for line in self.input:
+            x, y, dx, dy = re.findall(r'-?\d+', line)
+            robots.append({
+                "x": int(x),
+                "y": int(y),
+                "dx": int(dx),
+                "dy": int(dy)
+            })
+
+        w = 11 if self.test else 101
+        h = 7 if self.test else 103
+
+        def get_state():
+            return tuple((robot["x"], robot["y"]) for robot in robots)
+
+        min_size = (0, math.inf)
+
+        def write_image():
+            nonlocal min_size
+            img = np.zeros((h, w, 3), dtype=np.uint8)
+            for robot in robots:
+                img[robot["y"], robot["x"]] = [255, 255, 255]
+            buffer = io.BytesIO()
+            imageio.imwrite(buffer, img, format="png")
+            size = buffer.tell()
+            if size < min_size[1]:
+                min_size = (i, size)
+            buffer.close()
+
+        seen = set()
+        seen.add(get_state())
+        i = 0
+
+        while True:
+            i += 1
+            for robot in robots:
+                robot["x"] = (robot["x"] + robot["dx"]) % w
+                robot["y"] = (robot["y"] + robot["dy"]) % h
+
+            state = get_state()
+            if state in seen:
+                break
+            seen.add(state)
+            write_image()
+        return min_size[0]
+
 
 # Day14("test").run()
 Day14().run()
