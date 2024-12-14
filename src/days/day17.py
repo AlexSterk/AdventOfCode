1351b81e4a34f9e602660f28463142919f78248a
Alexander Sterk <ajhsterk@gmail.com>
2024-12-14 13:11:06 +0100

    simplify

--- src/days/day17.py
+++ src/days/day17.py
@@ -1,4 +1,5 @@
 import hashlib
+from collections import deque
 
 from src.setup.day import Day
 from src.util.dijkstra import shortest_path
@@ -15,6 +16,26 @@ grid = """#########
 ####### V""".split("\n")
 
 
+def get_open_doors(passcode, path):
+    return hashlib.md5((passcode + path).encode()).hexdigest()[:4]
+
+
+def neighbors(passcode):
+    def inner(node):
+        x, y, path = node
+        u, d, l, r = get_open_doors(passcode, path)
+        if u in "bcdef" and grid[y - 1][x] == "-":
+            yield x, y - 2, path + "U"
+        if d in "bcdef" and grid[y + 1][x] == "-":
+            yield x, y + 2, path + "D"
+        if l in "bcdef" and grid[y][x - 1] == "|":
+            yield x - 2, y, path + "L"
+        if r in "bcdef" and grid[y][x + 1] == "|":
+            yield x + 2, y, path + "R"
+
+    return inner
+
+
 class Day17(Day):
     @property
     def day(self):
@@ -23,29 +44,27 @@ class Day17(Day):
     @solution("DUDRDLRRRD")
     def part1(self) -> object:
         passcode = self.raw_input
-
-        def get_open_doors(path):
-            return hashlib.md5((passcode + path).encode()).hexdigest()[:4]
-
-        def neighbors(node):
-            x, y, path = node
-            u, d, l, r = get_open_doors(path)
-            if u in "bcdef" and grid[y - 1][x] == "-":
-                yield x, y - 2, path + "U"
-            if d in "bcdef" and grid[y + 1][x] == "-":
-                yield x, y + 2, path + "D"
-            if l in "bcdef" and grid[y][x - 1] == "|":
-                yield x - 2, y, path + "L"
-            if r in "bcdef" and grid[y][x + 1] == "|":
-                yield x + 2, y, path + "R"
-
-        _, end, _ = shortest_path((1, 1, ""), lambda x: x[0] == 7 and x[1] == 7, neighbors, lambda a, b: 1)
+        _, end, _ = shortest_path((1, 1, ""), lambda x: x[0] == 7 and x[1] == 7, neighbors(passcode), lambda a, b: 1)
 
         return end[2]
 
-    @solution("")
+    @solution("502")
     def part2(self) -> object:
-        return None
+        passcode = self.raw_input
+
+        _neighbors = neighbors(passcode)
+
+        q = deque([(1, 1, "")])
+        longest = 0
+        while q:
+            x, y, path = q.popleft()
+            if x == 7 and y == 7:
+                longest = max(longest, len(path))
+                continue
+            for n in _neighbors((x, y, path)):
+                q.append(n)
+
+        return longest
 
 
 # Day17("test").run()
