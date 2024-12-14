340440a35e6edaa44091b8690659435cae8f5e1b
Alexander Sterk <ajhsterk@gmail.com>
2024-12-14 01:28:21 +0100

    d14 p2

--- src/days/day14.py
+++ src/days/day14.py
@@ -1,8 +1,17 @@
+import functools
 import hashlib
 import re
 
 from src.setup.day import Day
 from src.util.solution import solution
+
+@functools.cache
+def get_hash(salt, index, stretch = 0):
+    key = salt + str(index)
+    h = hashlib.md5(key.encode()).hexdigest()
+    for _ in range(stretch):
+        h = hashlib.md5(h.encode()).hexdigest()
+    return h
 
 class Day14(Day):
     @property
@@ -10,27 +19,25 @@ class Day14(Day):
         return 14
 
     @solution("15035")
-    def part1(self) -> object:
+    def part1(self, stretch = 0) -> object:
         salt = self.raw_input
         i = 0
         keys = []
         while len(keys) < 64:
-            key = salt + str(i)
+            h = get_hash(salt, i, stretch)
             i += 1
-            h = hashlib.md5(key.encode()).hexdigest()
             if m := re.search(r'(.)\1\1', h):
                 m = m.group(1)
                 for j in range(i, i + 1000):
-                    key = salt + str(j)
-                    h = hashlib.md5(key.encode()).hexdigest()
+                    h = get_hash(salt, j, stretch)
                     if re.search(rf'({m})\1\1\1\1', h):
                         keys.append(i - 1)
                         break
-        return i - 1
+        return keys[-1]
 
-    @solution("")
+    @solution("19968")
     def part2(self) -> object:
-        return None
+        return self.part1(2016, skip_test=True)
 
 # Day14("test").run()
 Day14().run()
