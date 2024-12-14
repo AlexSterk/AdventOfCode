515ae7a13ae8cb19361a87797360672adfb9a6ad
Alexander Sterk <ajhsterk@gmail.com>
2024-12-14 13:38:44 +0100

    d18 p1

---
+++ src/days/day18.py
@@ -0,0 +1,36 @@
+from src.setup.day import Day
+from src.util.solution import solution
+
+class Day18(Day):
+    @property
+    def day(self):
+        return 18
+
+    @solution("1939")
+    def part1(self) -> object:
+        grid = {(x,y): c for y, row in enumerate(self.input) for x, c in enumerate(row)}
+        w = len(grid)
+
+        def next(x, y):
+            p1 = grid.get((x-1, y-1), ".")
+            p2 = grid.get((x, y-1), ".")
+            p3 = grid.get((x+1, y-1), ".")
+
+            if p1 == "^" and p2 == "^" and p3 == ".": return "^"
+            if p1 == "." and p2 == "^" and p3 == "^": return "^"
+            if p1 == "^" and p2 == "." and p3 == ".": return "^"
+            if p1 == "." and p2 == "." and p3 == "^": return "^"
+            return "."
+
+        for y in range(1, 40):
+            for x in range(w):
+                grid[(x,y)] = next(x, y)
+
+        return sum(1 for c in grid.values() if c == ".")
+
+    @solution("")
+    def part2(self) -> object:
+        return None
+
+# Day18("test").run()
+Day18().run()
