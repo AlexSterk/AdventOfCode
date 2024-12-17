4e0311894f98d8b73299ef348fe7a36c57a25533
Alexander Sterk <ajhsterk@gmail.com>
2024-12-17 20:49:21 +0100

    d17 p2

--- src/days/day17.py
+++ src/days/day17.py
@@ -46,73 +46,25 @@ class Day17(Day):
         program = self.raw_input.split("\n\n")[1]
         program = list(map(int, program.replace("Program: ", "").split(",")))
 
-        # For some reason. Z3 never finishes. I'm not sure why...
+        solver = Solver()
+        s = BitVec('s', 64)
+        solver.add(s > 0)
+        a, b, c = s, 0, 0
+        for x in program:
+            b = a % 8
+            b = b ^ 5
+            c = a / (1 << b)
+            b = b ^ c
+            b = b ^ 6
+            a = a / (1 << 3)
+            solver.add((b % 8) == x)
+        solver.add(a == 0)
 
-        # opt = Optimize()
-        # s = BitVec('s', 16**3)
-        # a, b, c = s, 0, 0
-        # for x in p:
-        #     b = a % 8
-        #     b = b ^ 5
-        #     c = a / (1 << b)
-        #     b = b ^ c
-        #     b = b ^ 6
-        #     a = a / (1 << 3)
-        #     opt.add((b % 8) == x)
-        # opt.add(a == 0)
-        # opt.add(s > 0)
-        # opt.minimize(s)
-        # opt.check()
-        # return opt.model().eval(s)
-
-        # direct copy from reddit, still hangs...
-        # opt = Optimize()
-        # s = BitVec('s', 64)
-        # a, b, c = s, 0, 0
-        # for x in [2,4,1,5,7,5,4,3,1,6,0,3,5,5,3,0]:
-        #     b = a % 8
-        #     b = b ^ 5
-        #     c = a / (1 << b)
-        #     b = b ^ c
-        #     b = b ^ 6
-        #     a = a / (1 << 3)
-        #     opt.add((b % 8) == x)
-        # opt.add(a == 0)
-        # opt.minimize(s)
-        # assert str(opt.check()) == 'sat'
-        # print(opt.model().eval(s))
-
-        # too lazy to try something else. so I stole this. no clue how it works
-        g = program
-        s= ""
-
-        def solve( p, r ):
-            nonlocal s
-            if p < 0:
-                s += str(r)
-                return True
-            for d in range( 8 ):
-                a, i = r << 3 | d, 0
-                while i < len( g ):
-                    if   g[ i + 1 ] <= 3: o = g[ i + 1 ]
-                    elif g[ i + 1 ] == 4: o = a
-                    elif g[ i + 1 ] == 5: o = b
-                    elif g[ i + 1 ] == 6: o = c
-                    if   g[ i ] == 0: a >>= o
-                    elif g[ i ] == 1: b  ^= g[ i + 1 ]
-                    elif g[ i ] == 2: b   = o & 7
-                    elif g[ i ] == 3: i   = g[ i + 1 ] - 2 if a != 0 else i
-                    elif g[ i ] == 4: b  ^= c
-                    elif g[ i ] == 5: w   = o & 7; break
-                    elif g[ i ] == 6: b   = a >> o
-                    elif g[ i ] == 7: c   = a >> o
-                    i += 2
-                if w == g[ p ] and solve( p - 1, r << 3 | d ):
-                    return True
-            return False
-
-        solve( len( program ) - 1, 0 )
-        return s
+        m = float('inf')
+        while solver.check() == sat:
+            m = min(m, solver.model().eval(s).as_long())
+            solver.add(s < m)
+        return m
 
 # Day17("test").run()
 Day17().run()
