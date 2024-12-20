edd66475357396abcb3b26670c7cb081ec77ac88
Alexander Sterk <ajhsterk@gmail.com>
2024-12-20 14:57:42 +0100

    d20 p1

--- src/days/day20.py
+++ src/days/day20.py
@@ -2,67 +2,57 @@ from functools import cache
 
 from src.setup.day import Day
 from src.util import directions
-from src.util.dijkstra import shortest_path, reconstruct_path, Dijkstra
+from src.util.dijkstra import shortest_path, reconstruct_path, Dijkstra, shortest_paths
 from src.util.solution import solution
+
 
 class Day20(Day):
     @property
     def day(self):
         return 20
 
-    @solution("")
+    @solution("1518")
     def part1(self) -> object:
-        grid = {(x,y): c for y, row in enumerate(self.input) for x, c in enumerate(row)}
+        grid = {(x, y): c for y, row in enumerate(self.input) for x, c in enumerate(row)}
         start = next(k for k, v in grid.items() if v == "S")
         end = next(k for k, v in grid.items() if v == "E")
 
         def ns(n):
-            x,y = n
+            x, y = n
             for dx, dy in directions.cardinal.values():
-                if grid.get((x+dx, y+dy), '#') != "#":
-                    yield x+dx, y+dy
+                if grid.get((x + dx, y + dy), '#') != "#":
+                    yield x + dx, y + dy
 
-        D = Dijkstra()
+        dist_from_start, _, _ = shortest_paths(start, ns)
+        dist_from_end, _, _ = shortest_paths(end, ns)
 
-        D.shortest_path(start, lambda n: n == end, ns)
-        no_cheats = D.d
-        path = D.get_shortest_path()
+        no_cheats = dist_from_start[end]
+        print(no_cheats)
 
-        @cache
-        def cached_shortest_path(n):
-            return shortest_path(n, lambda n: n == end, ns)
+        total = 0
 
-        cheats = {}
-        for i, (x,y) in enumerate(path):
-            for (dx,dy) in directions.cardinal.values():
-                ch_start = (x + dx, y + dy)
-                ch_end = (x + dx * 2, y + dy * 2)
-                if grid.get(ch_end, "#") != "#":
-                    _,_,d = cached_shortest_path(ch_end)
-                    if d:
-                        # print(f"When cheating from path position {i} ({x},{y}), we cheat from {ch_start} to {ch_end} and from there it takes {d} steps to reach the end")
-                        to_i = i
-                        from_i_to_ch = 2
-                        from_ch_to_end = d
-                        length = to_i + from_i_to_ch + from_ch_to_end
-                        saved = no_cheats - length
-                        # print(f"We save {saved} steps")
-                        assert (ch_start, ch_end) not in cheats
-                        cheats[(ch_start, ch_end)] = saved
-        # count all cheats that save more than 100 steps
-        return sum(1 for v in cheats.values() if v > 100)
+        def manhattan(a, b):
+            x1, y1 = a
+            x2, y2 = b
+            return abs(x1 - x2) + abs(y1 - y2)
 
-        # count values of saved_list
-        # saved_list = [v for v in cheats.values()]
-        # saved_list.sort()
-        # for v in set(saved_list):
-        #     print(f"Value {v} occurs {saved_list.count(v)} times")
-
-
+        for p, v in grid.items():
+            x, y = p
+            if v == "#":
+                continue
+            for (dx, dy) in directions.cardinal.values():
+                q = (x + dx * 2, y + dy * 2)
+                if grid.get(q, "#") == "#":
+                    continue
+                cheat = dist_from_start[p] + dist_from_end[q] + manhattan(p, q)
+                if no_cheats - cheat >= 100:
+                    total += 1
+        return total
 
     @solution("")
     def part2(self) -> object:
         return None
+
 
 # Day20("test").run()
 Day20().run()
