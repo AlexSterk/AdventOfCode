6a7a3469999c206a266d747ce5c543282ac69dae
Alexander Sterk <ajhsterk@gmail.com>
2024-12-20 15:09:11 +0100

    d20 p2 not working yet

--- src/days/day20.py
+++ src/days/day20.py
@@ -41,17 +41,52 @@ class Day20(Day):
             if v == "#":
                 continue
             for (dx, dy) in directions.cardinal.values():
-                q = (x + dx * 2, y + dy * 2)
-                if grid.get(q, "#") == "#":
-                    continue
-                cheat = dist_from_start[p] + dist_from_end[q] + manhattan(p, q)
-                if no_cheats - cheat >= 100:
-                    total += 1
+                for r in range(1,3):
+                    q = (x + dx * r, y + dy * r)
+                    if grid.get(q, "#") == "#":
+                        continue
+                    cheat = dist_from_start[p] + dist_from_end[q] + manhattan(p, q)
+                    if no_cheats - cheat >= 100:
+                        total += 1
         return total
 
     @solution("")
     def part2(self) -> object:
-        return None
+        grid = {(x, y): c for y, row in enumerate(self.input) for x, c in enumerate(row)}
+        start = next(k for k, v in grid.items() if v == "S")
+        end = next(k for k, v in grid.items() if v == "E")
+
+        def ns(n):
+            x, y = n
+            for dx, dy in directions.cardinal.values():
+                if grid.get((x + dx, y + dy), '#') != "#":
+                    yield x + dx, y + dy
+
+        dist_from_start, _, _ = shortest_paths(start, ns)
+        dist_from_end, _, _ = shortest_paths(end, ns)
+
+        no_cheats = dist_from_start[end]
+
+        total = 0
+
+        def manhattan(a, b):
+            x1, y1 = a
+            x2, y2 = b
+            return abs(x1 - x2) + abs(y1 - y2)
+
+        for p, v in grid.items():
+            x, y = p
+            if v == "#":
+                continue
+            for (dx, dy) in directions.cardinal.values():
+                for r in range(1,21):
+                    q = (x + dx * r, y + dy * r)
+                    if grid.get(q, "#") == "#":
+                        continue
+                    cheat = dist_from_start[p] + dist_from_end[q] + manhattan(p, q)
+                    if no_cheats - cheat >= 100:
+                        total += 1
+        return total
 
 
 # Day20("test").run()
