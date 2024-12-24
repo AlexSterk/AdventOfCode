0f9e704c0b2e54add15a63325aa3c958d4de436f
Alexander Sterk <ajhsterk@gmail.com>
2024-12-24 18:04:18 +0100

    d24 p2

--- src/days/day24.py
+++ src/days/day24.py
@@ -40,7 +40,7 @@ class Day24(Day):
         print(b)
         return int(b, 2)
 
-    @solution("")
+    @solution("gfv,hcm,kfs,tqm,vwr,z06,z11,z16")
     def part2(self) -> object:
         _, gates = self.raw_input.split("\n\n")
         pattern = r"(.+) (AND|XOR|OR) (.+) -> (.+)"
@@ -76,13 +76,13 @@ class Day24(Day):
                 z |= out[f"z{str(i).zfill(2)}"] << i
             return z
 
-        def eval_str(w, wires={}, l=0, max_l = 4):
+        def eval_str(w, wires={}, l=0, max_l=4):
             if w.startswith("x") or w.startswith("y") or l == max_l:
                 return w
             gate = next(gate for gate in gates if gate[-1] == w)
             w1, op, w2, o = gate
-            w1 = eval_str(w1, wires, l + 1)
-            w2 = eval_str(w2, wires, l + 1)
+            w1 = eval_str(w1, wires, l + 1, max_l)
+            w2 = eval_str(w2, wires, l + 1, max_l)
             o = "" if max_l == -1 else o
             if op == "AND":
                 wires[w] = f"{o}({w1} AND {w2})"
@@ -92,33 +92,40 @@ class Day24(Day):
                 wires[w] = f"{o}({w1} OR {w2})"
             return wires[w]
 
-        def swap_output(a,b):
+        swaps = []
+        def swap_output(a, b):
+            swaps.append(a)
+            swaps.append(b)
             a = next(gate for gate in gates if gate[-1] == a)
             b = next(gate for gate in gates if gate[-1] == b)
 
-            a1,a2,a3,a4 = a
-            b1,b2,b3,b4 = b
+            a1, a2, a3, a4 = a
+            b1, b2, b3, b4 = b
 
             gates.remove(a)
             gates.remove(b)
 
-            gates.append((a1,a2,a3,b4))
-            gates.append((b1,b2,b3,a4))
+            gates.append((a1, a2, a3, b4))
+            gates.append((b1, b2, b3, a4))
 
         swap_output("z06", "vwr")
         swap_output("z11", "tqm")
-        # swap_output("bjg", "bgg")
+        swap_output("z16", "kfs")
+        swap_output("hcm", "gfv")
 
         for i in range(z_len):
             print(f"z{str(i).zfill(2)} = {eval_str(f'z{str(i).zfill(2)}')}")
             n = 1 << i
             if add(n, n) != n + n:
-                print(f"mismatch at bit {i}")
+                print(f"mismatch at bit {i + 1}")
                 v = add(n, n)
                 print(f"add({n},{n}) = {v}")
                 break
-        for i in range(i+1,i+3):
-            print(f"z{str(i).zfill(2)} = {eval_str(f'z{str(i).zfill(2)}')}")
+        if i + 3 < z_len:
+            for i in range(i + 1, i + 3):
+                print(f"z{str(i).zfill(2)} = {eval_str(f'z{str(i).zfill(2)}')}")
+
+        return ",".join(sorted(swaps))
 
 
 # Day24("test").run()
