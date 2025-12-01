7c1449ff591387fc44ecbde86ceb88ae25a57f03
Alexander Sterk <ajhsterk@gmail.com>
2025-12-01 13:31:26 +0100

    init

---
+++ src/days/day1.py
@@ -0,0 +1,18 @@
+from src.setup.day import Day
+from src.util.solution import solution
+
+class Day1(Day):
+    @property
+    def day(self):
+        return 1
+
+    @solution("")
+    def part1(self) -> object:
+        return None
+
+    @solution("")
+    def part2(self) -> object:
+        return None
+
+Day1("test").run()
+# Day1().run()
--- src/setup/download.py
+++ src/setup/download.py
@@ -2,7 +2,7 @@ import os
 
 import requests
 
-YEAR = 2024
+YEAR = 2025
 
 print(f"Download input for day: ", end="")
 day = input()
