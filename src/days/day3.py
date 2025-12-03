68abdb2d879e578244af015406c0e3e9eb6bded9
Alexander Sterk <ajhsterk@gmail.com>
2025-12-03 22:28:19 +0100

    d3p2

--- src/days/day3.py
+++ src/days/day3.py
@@ -21,21 +21,21 @@ class Day3(Day):
 
     @solution("")
     def part2(self) -> object:
-        L = 12
-        @cache
-        def build_n(s, l):
-            if len(s) == L:
-                return int(s)
-            if l == "":
-                return 0
-            return max(build_n(s + l[0], l[1:]), build_n(s, l[1:]))
+        def joltage(bank, N=12):
+            num = []
+            idx = 0
+            L = len(bank)
+            for n in range(N):
+                m = max(bank[idx:L-N+n+1])
+                idx = bank.index(m, idx) + 1
+                num.append(m * 10**(N-1-n))
+            return sum(num)
 
-        res = 0
+
+        c = 0
         for line in self.read_input():
-            m = max(0, build_n("", line))
-            print(m)
-            res += m
-        return res
+            c += joltage([int(x) for x in line])
+        return c
 
 # Day3("test").run()
 Day3().run()
