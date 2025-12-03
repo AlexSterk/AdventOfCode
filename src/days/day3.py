898b89c4a8ee65f0afaf497333afa84c0486eab4
Alexander Sterk <ajhsterk@gmail.com>
2025-12-03 21:55:48 +0100

    d3p2 naive not working

--- src/days/day3.py
+++ src/days/day3.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from src.setup.day import Day
 from src.util.solution import solution
 
@@ -14,13 +16,26 @@ class Day3(Day):
             for i, x in enumerate(line[:-1]):
                 for y in line[i + 1:]:
                     m = max(m, int(x + y))
-            # print(m)
             res += m
         return res
 
     @solution("")
     def part2(self) -> object:
-        return None
+        L = 12
+        @cache
+        def build_n(s, l):
+            if len(s) == L:
+                return int(s)
+            if l == "":
+                return 0
+            return max(build_n(s + l[0], l[1:]), build_n(s, l[1:]))
+
+        res = 0
+        for line in self.read_input():
+            m = max(0, build_n("", line))
+            print(m)
+            res += m
+        return res
 
 # Day3("test").run()
 Day3().run()
