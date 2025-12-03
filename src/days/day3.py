8c99ce96209f75ca6c79c9d0a1edc15e110c5468
Alexander Sterk <ajhsterk@gmail.com>
2025-12-03 22:48:45 +0100

    improvements

--- src/days/day3.py
+++ src/days/day3.py
@@ -3,6 +3,18 @@ from functools import cache
 from src.setup.day import Day
 from src.util.solution import solution
 
+def joltage(bank, N):
+    on_batteries = bank[-N:] # get the last N batteries
+    bank = bank[:-N] # get all batteries to the left of last N
+    bank.reverse() # go in reverse order (i.e. rightmost first)
+    for bat in bank:
+        for i in range(N):
+            if bat >= on_batteries[i]: # can this battery replace the current battery in slot i
+                on_batteries[i], bat = bat, on_batteries[i] # swap them
+            else: break
+    joltage = int(''.join(map(str, on_batteries)))
+    return joltage
+
 class Day3(Day):
     @property
     def day(self):
@@ -12,30 +24,14 @@ class Day3(Day):
     def part1(self) -> object:
         res = 0
         for line in self.read_input():
-            m = 0
-            for i, x in enumerate(line[:-1]):
-                for y in line[i + 1:]:
-                    m = max(m, int(x + y))
-            res += m
+            res += joltage([int(x) for x in line], 2)
         return res
 
     @solution("172981362045136")
     def part2(self) -> object:
-        def joltage(bank, N=12):
-            num = ""
-            idx = 0
-            L = len(bank)
-            for n in range(N):
-                sub = bank[idx:L - N + n + 1]
-                m = max(sub)
-                idx = bank.index(m, idx) + 1
-                num += str(m)
-            return int(num)
-
-
         c = 0
         for line in self.read_input():
-            c += joltage([int(x) for x in line])
+            c += joltage([int(x) for x in line], 12)
         return c
 
 # Day3("test").run()
