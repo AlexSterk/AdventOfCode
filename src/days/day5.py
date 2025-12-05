5fc9693880752a02e3f63c6a8ffff0b4cba4f644
Alexander Sterk <ajhsterk@gmail.com>
2025-12-05 10:39:53 +0100

    d5p2

--- src/days/day5.py
+++ src/days/day5.py
@@ -8,10 +8,7 @@ class Day5(Day):
 
     @solution("623")
     def part1(self) -> object:
-        fresh_ranges, ingredients = self.input
-        fresh_ranges = [tuple(map(int, x.split("-"))) for x in fresh_ranges]
-        fresh_ranges = sorted(fresh_ranges)
-        ingredients = [int(x) for x in ingredients]
+        fresh_ranges, ingredients = self.parsed()
         # print(fresh_ranges, ingredients)
 
         fresh = 0
@@ -24,9 +21,35 @@ class Day5(Day):
                     break
         return fresh
 
-    @solution("")
+    def parsed(self):
+        fresh_ranges, ingredients = self.input
+        fresh_ranges = [tuple(map(int, x.split("-"))) for x in fresh_ranges]
+        fresh_ranges = sorted(fresh_ranges)
+        ingredients = [int(x) for x in ingredients]
+        return fresh_ranges, ingredients
+
+    @solution("353507173555373")
     def part2(self) -> object:
-        return None
+        ranges, _ = self.parsed()
+        print(ranges)
+
+        while True:
+            if self.loop(ranges):
+                continue
+            break
+
+        print(ranges)
+
+        return sum(high-low+1 for (low, high) in ranges)
+
+    def loop(self, ranges):
+        for i, cur_range in enumerate(ranges[:-1]):
+            next_range = ranges[i + 1]
+            if cur_range[0] <= next_range[0] <= cur_range[1]:
+                ranges[i] = (cur_range[0], max(cur_range[1], next_range[1]))
+                ranges.remove(next_range)
+                return True
+        return False
 
     def read_input(self):
         super().read_input()
