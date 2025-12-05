b10af2610bbd29487b071f6afa01cc40aba101ce
Alexander Sterk <ajhsterk@gmail.com>
2025-12-05 10:47:05 +0100

    improve looping

--- src/days/day5.py
+++ src/days/day5.py
@@ -1,6 +1,7 @@
 from src.setup.day import Day
 from src.util.solution import solution
 
+
 class Day5(Day):
     @property
     def day(self):
@@ -9,17 +10,26 @@ class Day5(Day):
     @solution("623")
     def part1(self) -> object:
         fresh_ranges, ingredients = self.parsed()
-        # print(fresh_ranges, ingredients)
 
-        fresh = 0
-        for ingredient in ingredients:
-            for low, high in fresh_ranges:
-                if low > ingredient:
-                    break
-                if low <= ingredient <= high:
-                    fresh += 1
-                    break
-        return fresh
+        return sum(
+            any(low <= ingredient <= high for low, high in fresh_ranges)
+            for ingredient in ingredients
+        )
+
+    @solution("353507173555373")
+    def part2(self) -> object:
+        ranges, _ = self.parsed()
+
+        merged = [ranges[0]]
+
+        for low, high in ranges[1:]:
+            last_low, last_high = merged[-1]
+            if last_low <= low <= last_high:
+                merged[-1] = (last_low, max(last_high, high))
+            else:
+                merged.append((low, high))
+
+        return sum(high - low + 1 for (low, high) in merged)
 
     def parsed(self):
         fresh_ranges, ingredients = self.input
@@ -28,33 +38,9 @@ class Day5(Day):
         ingredients = [int(x) for x in ingredients]
         return fresh_ranges, ingredients
 
-    @solution("353507173555373")
-    def part2(self) -> object:
-        ranges, _ = self.parsed()
-        print(ranges)
-
-        while True:
-            if self.loop(ranges):
-                continue
-            break
-
-        print(ranges)
-
-        return sum(high-low+1 for (low, high) in ranges)
-
-    def loop(self, ranges):
-        for i, cur_range in enumerate(ranges[:-1]):
-            next_range = ranges[i + 1]
-            if cur_range[0] <= next_range[0] <= cur_range[1]:
-                ranges[i] = (cur_range[0], max(cur_range[1], next_range[1]))
-                ranges.remove(next_range)
-                return True
-        return False
-
     def read_input(self):
         super().read_input()
         return [x.split("\n") for x in self.raw_input.split("\n\n")]
-
 
 
 # Day5("test").run()
