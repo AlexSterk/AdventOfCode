509e06bd6b1104d3bcd76b7c5390d14a001e309c
Alexander Sterk <ajhsterk@gmail.com>
2025-12-06 13:59:48 +0100

    update p1 with comments

--- src/days/day6.py
+++ src/days/day6.py
@@ -28,37 +28,46 @@ class Day6(Day):
 
     @solution("5595593539811")
     def part1(self) -> object:
-        data = re.split(r"\s+", self.raw_input.strip().removesuffix("\n"))
-        for i, s in enumerate(data):
-            if re.match(r"[*+]", s):
-                break
-        number_of_equations = len(data[i:])
-        terms_per_equation = i // number_of_equations
+        # Split into list of terms, don't care about new lines for now
+        tokens = re.split(r"\s+", self.raw_input.strip().removesuffix("\n"))
+        # find the first index of an operand, that is where the ints end
+        first_operand_index = next(i for i,s in enumerate(tokens) if re.match(r"[*+]", s))
+        # calculate how many operands, and therefore, equations there are
+        number_of_equations = len(tokens) - first_operand_index
+        # calculate how many terms each equation has
+        terms_per_equation = first_operand_index // number_of_equations
+        # build equation list
         equations = []
         for i in range(number_of_equations):
-            terms = data[i:i + number_of_equations * (terms_per_equation + 1):number_of_equations]
-            operand = terms.pop()
+            # for each equation, get all the terms
+            terms = tokens[i:first_operand_index:number_of_equations]
+            # get the operand
+            operand = tokens[first_operand_index + i]
+            # map terms
             terms = [int(s) for s in terms]
+            # convert to equation
             equations.append(Equation(operand, terms))
 
+        # eval equations
         return sum(equation.eval() for equation in equations)
 
     @solution("10153315705125")
     def part2(self) -> object:
-        lines = self.input[:-1]
-        operands = self.input[-1]
-        i = 0
+        term_lines = self.input[:-1]
+        operand_line = self.input[-1]
+
         equations = []
-        for operand in re.findall(r"[*+]\s*", operands):
-            i = operands.find(operand, i)
+        last_index = 0
+        for op_match in re.findall(r"[*+]\s*", operand_line):
+            last_index = operand_line.find(op_match, last_index)
             terms = []
-            for line in lines:
-                end = i + len(operand)
-                if len(operand) == 1:
-                    end=i + len(line)
-                term = line[i:end].removesuffix(" ")
+            for line in term_lines:
+                end = last_index + len(op_match)
+                if len(op_match) == 1:
+                    end=last_index + len(line)
+                term = line[last_index:end].removesuffix(" ")
                 terms.append(term)
-            i += 1
+            last_index += 1
             max_l = len(max(terms, key=len))
             int_terms = []
             for j in range(max_l):
@@ -66,7 +75,7 @@ class Day6(Day):
                 s = str.join("", s).strip()
                 if len(s) > 0:
                     int_terms.append(int(s))
-            e = Equation(operand.strip(), int_terms)
+            e = Equation(op_match.strip(), int_terms)
             equations.append(e)
 
         return sum(equation.eval() for equation in equations)
