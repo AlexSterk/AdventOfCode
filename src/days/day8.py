13f35e4fe27b0aa6c4e5ce68eb9a1f0428157b8b
Alexander Sterk <ajhsterk@gmail.com>
2025-12-08 18:34:14 +0100

    d8p2

--- src/days/day8.py
+++ src/days/day8.py
@@ -41,9 +41,31 @@ class Day8(Day):
 
         return len(one) * len(two) * len(three)
 
-    @solution("")
+    @solution("8141888143")
     def part2(self) -> object:
-        return None
+        boxes = {
+            coords: {coords}
+            for line in self.input
+            if (coords := tuple(map(int, line.split(","))))
+        }
+
+        distances = {}
+        for box1, box2 in combinations(boxes, 2):
+            distances[box1, box2] = np.linalg.norm(np.array(box1) - np.array(box2))
+
+        sorted_distances = sorted(distances.items(), key=lambda x: x[1])
+        i = 0
+        while True:
+            closest = sorted_distances[i][0]
+            p1, p2 = closest
+            merged = boxes[p1].union(boxes[p2])
+            for p in merged:
+                boxes[p] = merged
+            if len(merged) == len(boxes):
+                break
+            i += 1
+
+        return p1[0] * p2[0]
 
 # Day8("test").run()
 Day8().run()
