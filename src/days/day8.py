bdbb90d01f32b99bb2e982815c83079785d654c3
Alexander Sterk <ajhsterk@gmail.com>
2025-12-08 18:43:04 +0100

    optimize a little

--- src/days/day8.py
+++ src/days/day8.py
@@ -1,10 +1,20 @@
-from collections import deque
-from itertools import product, combinations
+from functools import cache
+from functools import cache
+from itertools import combinations
 
 import numpy as np
 
 from src.setup.day import Day
 from src.util.solution import solution
+
+
+def merge_boxes(boxes, i, sorted_distances):
+    closest = sorted_distances[i][0]
+    p1, p2 = closest
+    merged = boxes[p1].union(boxes[p2])
+    for p in merged:
+        boxes[p] = merged
+    return merged, p1, p2
 
 
 class Day8(Day):
@@ -14,24 +24,10 @@ class Day8(Day):
 
     @solution("46398")
     def part1(self) -> object:
-        boxes = {
-            coords: {coords}
-            for line in self.input
-            if (coords := tuple(map(int, line.split(","))))
-        }
-
-        distances = {}
-        for box1, box2 in combinations(boxes, 2):
-            distances[box1, box2] = np.linalg.norm(np.array(box1) - np.array(box2))
-
-        sorted_distances = sorted(distances.items(), key=lambda x: x[1])
+        boxes, sorted_distances = self.set_up()
         runs = 10 if self.test else 1000
         for i in range(runs):
-            closest = sorted_distances[i][0]
-            p1, p2 = closest
-            merged = boxes[p1].union(boxes[p2])
-            for p in merged:
-                boxes[p] = merged
+            merge_boxes(boxes, i, sorted_distances)
 
         circuits = set(frozenset(s) for s in boxes.values())
         # print(circuits)
@@ -43,29 +39,29 @@ class Day8(Day):
 
     @solution("8141888143")
     def part2(self) -> object:
-        boxes = {
-            coords: {coords}
-            for line in self.input
-            if (coords := tuple(map(int, line.split(","))))
-        }
-
-        distances = {}
-        for box1, box2 in combinations(boxes, 2):
-            distances[box1, box2] = np.linalg.norm(np.array(box1) - np.array(box2))
-
-        sorted_distances = sorted(distances.items(), key=lambda x: x[1])
+        boxes, sorted_distances = self.set_up()
         i = 0
         while True:
-            closest = sorted_distances[i][0]
-            p1, p2 = closest
-            merged = boxes[p1].union(boxes[p2])
-            for p in merged:
-                boxes[p] = merged
+            merged, p1, p2 = merge_boxes(boxes, i, sorted_distances)
             if len(merged) == len(boxes):
                 break
             i += 1
 
         return p1[0] * p2[0]
+
+    @cache
+    def set_up(self):
+        boxes = {
+            coords: {coords}
+            for line in self.input
+            if (coords := tuple(map(int, line.split(","))))
+        }
+        distances = {}
+        for box1, box2 in combinations(boxes, 2):
+            distances[box1, box2] = np.linalg.norm(np.array(box1) - np.array(box2))
+        sorted_distances = sorted(distances.items(), key=lambda x: x[1])
+        return boxes, sorted_distances
+
 
 # Day8("test").run()
 Day8().run()
