159e379d0611d5e240acccdbe3f5da4724b52114
Alexander Sterk <ajhsterk@gmail.com>
2025-12-10 20:26:47 +0100

    commit what I have

--- src/days/day10.py
+++ src/days/day10.py
@@ -1,6 +1,5 @@
-from collections import deque
-
 import numpy as np
+from scipy.optimize import linprog
 
 from src.setup.day import Day
 from src.util.dijkstra import shortest_path
@@ -8,12 +7,13 @@ from src.util.solution import solution
 
 ld = {".": 0, "#": 1}
 
+
 def button_to_bitstring(button: str, size: int):
     button = button.strip("()").split(",")
     button = [int(x) for x in button]
-    result = [False] * size
+    result = [0] * size
     for b in button:
-        result[b] = True
+        result[b] = 1
     return result
 
 
@@ -27,26 +27,42 @@ class Day10(Day):
         total = 0
         for line in self.input:
             end, *buttons, _ = line.split(" ")
-            end = tuple([True if c == "#" else False for c in end.strip("[]")])
+            end = tuple([1 if c == "#" else 0 for c in end.strip("[]")])
             buttons = [button_to_bitstring(b, len(end)) for b in buttons]
             # print(end, buttons)
 
-            start = [False] * len(end)
+            start = [0] * len(end)
+
             def is_end(cur):
                 return cur == end
+
             def neighbours(cur):
                 for b in buttons:
                     yield tuple((np.array(cur) ^ np.array(b)).tolist())
+
             total += shortest_path(tuple(start), is_end, neighbours)[2]
         return total
 
-
-
-
     @solution("")
     def part2(self) -> object:
-        return None
+        total = 0
+        for line in self.input:
+            _, *buttons, joltage = line.split(" ")
+            joltage = [int(x) for x in joltage.strip("{}").split(",")]
+            buttons = [np.array(button_to_bitstring(b, len(joltage))) for b in buttons]
+            cost = np.ones(len(buttons))
+            buttons = np.vstack(buttons).T
+
+            joltage = np.array(joltage)
+            # print(joltage, buttons, cost)
+
+            result = linprog(c=cost, A_eq=buttons, b_eq=joltage, method='highs')
+            result = result.x
+            print(result)
+            total += np.sum(result)
+
+        return total, int(total)
 
 
-# Day10("test").run()
-Day10().run()
+Day10("test").run()
+# Day10().run()
