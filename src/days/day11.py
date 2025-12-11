d84ff4782f9b580002cc560cd842af87559412e3
Alexander Sterk <alexander.sterk@technolution.nl>
2025-12-11 15:27:06 +0100

    clean up code

--- src/days/day11.py
+++ src/days/day11.py
@@ -1,59 +1,38 @@
-from _pyrepl.commands import end
-from functools import cache
+from collections import defaultdict
 
 from src.setup.day import Day
-from src.util.dijkstra import all_paths, count_paths
+from src.util.dijkstra import count_paths
 from src.util.solution import solution
 
 
 class Day11(Day):
+    devices = {}
+
     @property
     def day(self):
         return 11
 
+    def ns(self, cur):
+        for n in self.devices[cur]:
+            yield n
+
+    def count(self, start, end):
+        return count_paths(start, lambda cur: cur == end, self.ns)
+
     @solution("615")
     def part1(self) -> object:
-        devices = {}
+        self.devices = defaultdict(list)
         for line in self.input:
             device_name, outputs = line.split(": ")
-            devices[device_name] = outputs.split(" ")
+            self.devices[device_name] = outputs.split(" ")
 
-        if "you" not in devices:
-            return None
-
-        start = ("you",)
-        end = "out"
-
-        def is_end(cur):
-            *_, cur = cur
-            return cur == end
-
-        def ns(cur):
-            *past, cur = cur
-            for n in devices[cur]:
-                yield *past, cur, n
-
-        return len(all_paths(start, is_end, ns))
+        return self.count("you", "out")
 
     @solution("303012373210128")
     def part2(self) -> object:
-        devices = {}
-        for line in self.input:
-            device_name, outputs = line.split(": ")
-            devices[device_name] = outputs.split(" ")
-        if "svr" not in devices:
-            return None
-
-        def ns(cur):
-            for n in devices.get(cur, []):
-                yield n
-
-        def count(start, end):
-            return count_paths(start, lambda cur: cur == end, ns)
-
-        return count("svr", "fft") * count("fft", "dac") * count("dac", "out")
+        return self.count("svr", "fft") * self.count("fft", "dac") * self.count("dac", "out")
 
 
-Day11("test").run()
+# Day11("test").run()
 # Day11("test2").run()
 Day11().run()
